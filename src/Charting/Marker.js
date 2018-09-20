d51c461d3c6d82e24313fe93bdea53d6a753a889
Vincent <jan-vincent.wyszynski@charite.de>
2018-09-21 00:46:26 +0200

    make Marker selectable to show paths and figures

--- src/Charting/Marker.js
+++ src/Charting/Marker.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Label from './Label'
 import { line } from 'd3-shape'
 
-const Marker = props => (
-    <g className="marker">
-        <circle 
-            className="dot" 
-            cx={props.xScale(props.x)} 
-            cy={props.yScale(props.y)} 
-            fill={props.color || 'black'}
-            r={props.size || "5"}
-        />
-        <path d={line()([[props.xScale(props.x), props.yScale(props.y)],[props.xScale(props.x), props.yScale(props.yScale.domain()[0])]])} className="line" style={{stroke: props.color || "black"}} />
-        <text alignmentBaseline="central" x={props.xScale(props.xScale.domain()[0]) - 2} y={props.yScale(props.y)} textAnchor="end">{props.y}</text>
-        <path d={line()([[props.xScale(props.xScale.domain()[0]), props.yScale(props.y)],[props.xScale(props.x), props.yScale(props.y)]])} className="line" style={{stroke: props.color || "black"}} />
-        <text alignmentBaseline="before-edge" x={props.xScale(props.x)} y={props.yScale(props.yScale.domain()[0])} textAnchor="middle">{props.x}</text>
-        <Label x={props.xScale(props.x)} y={props.yScale(props.y) + (props.offset || -20)}>{props.label}</Label>
-    </g>
-)
+class Marker extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selected: false
+        }
+    } 
+
+    toggleSelected() {
+        this.setState({ selected: !this.state.selected })
+    }
+
+    render() {
+        return (
+            <g className={`marker ${this.state.selected ? 'selected' : ''}`}>
+                <circle 
+                    className={`dot ${this.state.selected ? 'selected' : ''}`}
+                    cx={this.props.xScale(this.props.x)} 
+                    cy={this.props.yScale(this.props.y)} 
+                    fill={this.props.color || 'black'}
+                    r={this.props.size || "5"}
+                    onClick={() => this.toggleSelected()}
+                />
+                <path d={line()([[this.props.xScale(this.props.x), this.props.yScale(this.props.y)],[this.props.xScale(this.props.x), this.props.yScale(this.props.yScale.domain()[0])]])} className="line" style={{stroke: this.props.color || "black"}} />
+                <text alignmentBaseline="central" x={this.props.xScale(this.props.xScale.domain()[0]) - 2} y={this.props.yScale(this.props.y)} textAnchor="end">{this.props.y}</text>
+                <path d={line()([[this.props.xScale(this.props.xScale.domain()[0]), this.props.yScale(this.props.y)],[this.props.xScale(this.props.x), this.props.yScale(this.props.y)]])} className="line" style={{stroke: this.props.color || "black"}} />
+                <text alignmentBaseline="before-edge" x={this.props.xScale(this.props.x)} y={this.props.yScale(this.props.yScale.domain()[0])} textAnchor="middle">{this.props.x}</text>
+                <Label onClick={() => this.toggleSelected()} x={this.props.xScale(this.props.x)} y={this.props.yScale(this.props.y) + (this.props.offset || -20)}>{this.props.label}</Label>
+            </g>
+        )
+    }
+}
 
 export default Marker
