064deb12977de390d88ad664841977bd183c9847
Vincent <jan-vincent.wyszynski@charite.de>
2018-09-21 00:31:12 +0200

    add grouped BarGraphs Charts as module histograms in Semester view

--- src/Charting/BarGraph.js
+++ src/Charting/BarGraph.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-export default function BarGraph(props) {
-	const bars = props.data.map((d, i) => <rect 
-		style={{fill: props.color || (d.highlight ? '#fe99f2' : '#fe9922')}} 
-		x={props.xScale(d.x)} 
-		y={props.yScale(d.y)}
-		height={props.yScale(0) - props.yScale(d.y)}
-		width={(props.xScale(1) - props.xScale(0)) * (props.width || .8)}
-		key={"bar" + i} ></rect>)
-	return <g transform={`translate(-${props.xScale(1) * (props.width || .8) / 2}, 0)`}>{bars}</g>
+const BarGraph = props => {
+	return (<g transform={`translate(-${props.xScale(props.width || 1) / 2}, 0)`}>{props.data.map((d, i) => 
+		<rect 
+			style={{fill: d.highlight ? (props.highlightColor || '#fe99f2') : (props.color || '#fe9922')}} 
+			x={props.xScale(d.x + (props.offset || 0))} 
+			y={props.yScale(d.y)}
+			height={props.yScale(props.yScale.domain()[0]) - props.yScale(d.y)}
+			width={props.xScale(props.width || 1)}
+			key={"bar" + i} />)}
+	</g>)
 }
+
+export default BarGraph
--- src/Exams/Semester.js
+++ src/Exams/Semester.js
@@ -5,6 +5,7 @@ import AreaGraph from '../Charting/AreaGraph'
 import _ from 'lodash'
 import Marker from '../Charting/Marker';
 import BarGraph from '../Charting/BarGraph';
+import HorizontalBarGraph from '../Charting/HorizontalBarGraph';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
@@ -15,18 +16,22 @@ function randn_bm() {
     return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
 }
 
-const result = Math.floor(Math.random() * 60 + 25)
-const data = new Array(100).fill(0).map(d => Math.floor(randn_bm() * 13 + 50)).concat([ result ]).sort((a,b) => a-b).map((d,i) => ({x: (i+1)/1.01, y: d}))
-const histo = _.map(_.groupBy(data, d => Math.floor(d.y / 5)), (d, i) => ({x: +i*5, y: d.length, highlight: +i === Math.floor(result / 5)}))
-const mean = Math.round(data.reduce((s, d) => s + d.y, 0) / data.length)
+function mean(x) {
+    return Math.round(x.reduce((s, d) => s+d, 0) / x.length)
+}
 
+const result = new Array(4).fill(0).map(() => Math.floor(Math.random() * 60 + 25))
+const data = new Array(100).fill(0).map(() => new Array(4).fill(0).map(() => Math.floor(randn_bm() * 13 + 50))).concat([ result ]).sort((a,b) => mean(a)-mean(b))
+const graphData = data.map((d,i) => ({x: (i+1)/1.01, y: mean(d)}))
+const histo = _.zip(...data).map((m,module) => _.map(_.groupBy(m, d => Math.floor(d / 5)), (d, i) => ({x: +i*5, y: d.length, highlight: +i === Math.floor(result[module] / 5)})))
+console.log(histo)
 function toAreaData(_data) {
-    return _data.map(d => ({x: d.x, y0: Math.min(...data.map(d => d.y)), y1: d.y}))
+    return _data.map(d => ({x: d.x, y0: Math.min(...graphData.map(d => d.y)), y1: d.y}))
 }
 
 function percentileArea(from, to) {
-    return toAreaData([{x: from, y: data.find(d => d.x >= from).y}]
-            .concat(data.filter(d => d.x >= from && d.x <= to), [{x: to, y: data.find(d => d.x >= to).y}]))
+    return toAreaData([{x: from, y: graphData.find(d => d.x >= from).y}]
+            .concat(graphData.filter(d => d.x >= from && d.x <= to), [{x: to, y: graphData.find(d => d.x >= to).y}]))
 }
 
 class Semester extends Component {
@@ -34,7 +39,7 @@ class Semester extends Component {
         super(props)
         this.match = match
         this.state = {
-            mode: 'graph'
+            mode: 'histo'
         }
     }
 
@@ -62,19 +67,28 @@ class Semester extends Component {
                             </div>
                             <div className="mt-3">
                                 {this.state.mode === 'graph' ? (
-                                <Chart ticks={{y:4}} xDomain={[0,100]} yDomain={[Math.min(...data.map(d => d.y)), Math.max(...data.map(d => d.y))]}>
-                                    <AreaGraph noSmooth data={percentileArea(90, 100)} color="hsla(217, 100%, 81%, .3)"></AreaGraph>
-                                    <AreaGraph noSmooth data={percentileArea(75, 90)} color="hsla(217, 100%, 63%, .3)"></AreaGraph>
-                                    <AreaGraph noSmooth data={percentileArea(50, 75)} color="hsla(217, 100%, 43%, .3)"></AreaGraph>
-                                    <LineGraph data={data} color="hsla(181, 100%, 41%, .6)" noPoints noSmooth/>
-                                    <Marker x={_.findLastIndex(data, d => d.y === result)} y={result} label='Du' color="hsla(0, 100%, 30%, .6)" />
-                                    <Marker x={_.findIndex(data, d => d.y >= mean)} y={mean} label='Mittelwert' color="hsla(0, 100%, 30%, .6)" />
+                                <Chart ticks={{y:4}} xDomain={[0,100]} yDomain={[Math.min(...graphData.map(d => d.y)), Math.max(...graphData.map(d => d.y))]}>
+                                    <AreaGraph noSmooth data={percentileArea(90, 100)} color="hsla(120, 100%, 80%, .2)"></AreaGraph>
+                                    <AreaGraph noSmooth data={percentileArea(75, 90)} color="hsla(120, 100%, 60%, .2)"></AreaGraph>
+                                    <AreaGraph noSmooth data={percentileArea(50, 75)} color="hsla(120, 100%, 40%, .2)"></AreaGraph>
+                                    <AreaGraph noSmooth data={percentileArea(0, 50)} color="hsla(120, 100%, 20%, .2)"></AreaGraph>
+                                    <LineGraph data={graphData} color="hsla(181, 100%, 41%, .6)" noPoints noSmooth/>
+                                    <Marker x={_.findLastIndex(graphData, d => d.y === mean(result))} y={mean(result)} label='Du' color="hsla(0, 100%, 30%, .6)" />
+                                    <Marker x={_.findIndex(graphData, d => d.y >= mean(graphData.map(d => d.y)))} y={mean(graphData.map(d => d.y))} label='Mittelwert' color="hsla(0, 100%, 30%, .6)" />
                                 </Chart>
                                 ) : (
-                                <Chart xDomain={[0,100]} yDomain={[0,Math.max(...histo.map(d => d.y))]}>
-                                    <BarGraph width={3.5} data={histo} />
+                                <Chart xDomain={[0,100]} yDomain={[0,Math.max(...histo.map(d => Math.max(...d.map(y => y.y))))]}>
+                                    <BarGraph offset={-1.5} width={1} data={histo[0]} color="hsla(33, 100%, 20%, .5)" highlightColor="hsla(33, 100%, 20%, .8)" />
+                                    <BarGraph offset={-0.5} width={1} data={histo[1]} color="hsla(33, 100%, 40%, .5)" highlightColor="hsla(33, 100%, 40%, .8)" />
+                                    <BarGraph offset={0.5} width={1} data={histo[2]} color="hsla(33, 100%, 60%, .5)" highlightColor="hsla(33, 100%, 60%, .8)" />
+                                    <BarGraph offset={1.5} width={1} data={histo[3]} color="hsla(33, 100%, 80%, .5)" highlightColor="hsla(33, 100%, 80%, .8)" />
                                 </Chart>
                                 )}
+                            </div>
+                            <div className="mt-3">
+                                <Chart horizontal xDomain={[0,100]} yDomain={[0,5]} ticks={{x: 11, y: 6, yFormat: d => 'M0' + d }}>
+                                    <HorizontalBarGraph data={result} />
+                                </Chart>
                             </div>
                         </div>
                     </div>
