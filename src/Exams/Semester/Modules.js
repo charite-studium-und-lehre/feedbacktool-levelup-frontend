54cf1828e1b0db45b836bcf5e7da40f97d9e6c8c
Vincent <jan-vincent.wyszynski@charite.de>
2018-09-26 23:26:22 +0200

    clean some code

--- src/Exams/Semester/Modules.js
+++ src/Exams/Semester/Modules.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import _ from 'lodash'
 import Chart from '../../Charting/Chart'
 import BarGraph from '../../Charting/BarGraph'
 import LineMarker from '../../Charting/LineMarker'
--- src/Exams/Semester/Semester.js
+++ src/Exams/Semester/Semester.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import _ from 'lodash'
 import Totals from './Totals'
 import Modules from './Modules'
