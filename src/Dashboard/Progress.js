05088bcba78fd5fba673bd1c3157e59038f9c33c
roaromey <43957596+roaromey@users.noreply.github.com>
2018-10-24 14:56:07 +0200

    Update Progress.js

--- src/Dashboard/Progress.js
+++ src/Dashboard/Progress.js
@@ -5,11 +5,11 @@ import LineGraph from '../Charting/LineGraph';
 const Progress = props => (
     <div className="card progress-card">
         <div className="card-body">
-            <h5 className="card-title">Dein Fortschritt im Studium</h5>
+            <h5 className="card-title">Dein Studienfortschritt</h5>
             <div className="card-text">
-            Hier kommt ein Fortschrittsbalken mit Meilensteinen.
+            Hier siehst Du deinen Studienfortschritt und deine bereits erreichten Meilensteinen.
             </div>
-            <LinearChart xDomain={[1,8]} yDomain={[0,2]}>
+            <LinearChart xDomain={[1,10]} yDomain={[0,2]}>
                 <LineGraph withLabels data={new Array(4).fill(1).map((d,i) => ({x: i+5, y: d, label: `${i+5}. Semester`}))} color="gray"></LineGraph>
                 <LineGraph withLabels data={new Array(5).fill(1).map((d,i) => ({x: i+1, y: d, label: `${i+1}. Semester`}))} color="limegreen"></LineGraph>
             </LinearChart>
