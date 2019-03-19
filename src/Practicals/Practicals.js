336acec74fe49502b77851fbd89e3aaf8aa32192
towma <m.towma@hotmail.com>
2019-03-19 17:11:53 +0100

    Aerzlich1 TaetigKeit

--- src/Practicals/Practicals.js
+++ src/Practicals/Practicals.js
@@ -3,19 +3,6 @@ import React, { Component } from 'react'
 import PracticalsItem from './PracticalsItem'
 import D3chart from "./scaleLine"
 import Checkbox from '../Exams/Checkbox'
-import MyDropdown from "./PracticalsItem"
-// import Example from "./slidedown"
-
-
-import { LinearChart } from './LinearChart';
-import LineGraph from '../Charting/LineGraph'
-
-import { XAxis, YAxis } from '../Charting/Axis'
-
-
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // ES6
-
-
 
 
 
@@ -23,9 +10,38 @@ class Practicals extends Component {
 
     render() {
         return (
-            
-            <D3chart/>
-           
+            <div className="container-fluid">
+                <div className="Practicals">
+                    <div className="row">
+                        <div className="col-md-9">
+                            <D3chart />
+                        </div>
+                        <div className="col-md-3">
+                              <PracticalsItem/>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="row">
+                <div className=" row col-md-9">
+                <div className="col-md">
+                <Checkbox label="Ich habe"/>
+                        </div>
+                        <div className="col-md">
+                <Checkbox label="Ich Kann"/>
+                        </div>
+                        <div className="col-md">
+                <Checkbox label="Ich Kann"/>
+                        </div>
+                        <div className="col-md">
+                <Checkbox label="Ich Kann"/>
+                        </div>
+                        </div>
+                       
+                </div>
+            </div>
+
+
         )
     }
 }
--- src/Practicals/PracticalsItem.js
+++ src/Practicals/PracticalsItem.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import Dropdown from 'react-bootstrap/Dropdown'
 import {SlideDown} from 'react-slidedown'
-import 'react-slidedown/lib/slidedown.css'
 import "./practicals.css"
+import Button from 'react-bootstrap/Button';
 
 
 export function MyDropdown(props) {
@@ -15,14 +15,62 @@ export function MyDropdown(props) {
 
 
 class PracticalsItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      open1: false,
+      open2: false,
+      open3:false,
+      open4: false,
+      open5: false
+
+     };
+    this.handleClick = this.handleClick.bind(this)
+}
+
+handleClick() {
+  this.setState({
+     open1: ! this.state.open1,
+     open2: ! this.state.open2,
+     open3: ! this.state.open3,
+     open4: ! this.state.open4,
+     open5: ! this.state.open5,
+    
+    });
+}
+
     render() {
         return ( 
         <div className="PracticalsItem">
-               <MyDropdown>
-                   <div >
-                       <h2></h2>
-                   </div>
-               </MyDropdown>
+       <div class="btn-group-vertical">
+       <button  type="button" className="btn btn-secondary" onClick={this.handleClick} >1. Betreuung von Patienten </button>
+       <MyDropdown  open={this.state.open1}>
+            <h4>Hallo</h4>
+          </MyDropdown>
+       <button type="button" className="btn btn-secondary">2. Ärztliche Prozeduren 1</button>
+       <MyDropdown open={this.state.open2}>
+            <h4>Hallo</h4>
+          </MyDropdown>
+          <button type="button" className="btn btn-secondary">3. Kommunikation mit Patienten</button>
+       <MyDropdown open={this.state.open3}>
+            <h4>Hallo</h4>
+          </MyDropdown>
+          <button type="button" className="btn btn-secondary">4. Kommunikation und Zusammenarbeit mit Kollegen</button>
+       <MyDropdown open={this.state.open4}>
+            <h4>Hallo</h4>
+          </MyDropdown>
+          <button type="button" className="btn btn-secondary">5. Weitere ärztliche professionelle Tätigkeit</button>
+       <MyDropdown open={this.state.open5}>
+            <h4>Hallo</h4>
+          </MyDropdown>
+
+         </div>
+             
+                  
+
+     
+                   
+               
                
         </div>
         )
