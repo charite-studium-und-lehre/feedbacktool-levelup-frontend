e93ce59b24b36d2cf1c7f27a320ebede22d64cc9
towma <m.towma@hotmail.com>
2019-03-20 15:20:23 +0100

    Arzliche Tatigkeiten

---
+++ src/Practicals/PracticalCheckbox.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import Checkbox from '../Exams/Checkbox'
+
+const PracticalCheckbox = props => (
+
+  <div className="row">
+
+    <div className="col-md">
+                <Checkbox label="Ich habe"/>
+                        </div>
+                        <div className="col-md">
+                <Checkbox label="Ich Kann"/>
+                        </div>
+                        <div className="col-md">
+                <Checkbox label="Fremdeinschätzung"/>
+                        </div>
+                        <div className="col-md">
+                <Checkbox label="Praktische Prüfungen"/>
+                        </div>
+  </div>
+)
+
+export default PracticalCheckbox
