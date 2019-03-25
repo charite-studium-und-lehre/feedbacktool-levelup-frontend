b41b6b98bd464774589b9f536df45ff087cf07b4
towma <m.towma@hotmail.com>
2019-03-25 15:53:59 +0100

    move extendable logic from Legend component to seperate HOC

--- src/Core/makeExtendable.js
+++ src/Core/makeExtendable.js
@@ -4,16 +4,23 @@ const makeExtendable = WrappedComponent => {
     return class Extendable extends Component {
         constructor(props) {
             super(props)
-            this.state = { extended: false }
+            this.state = { extended: window.innerWidth < 768 }
+            this.element = React.createRef()
+        }
+
+        componentDidMount() {
+            this.originalHeight = this.element.current.offsetHeight
+            this.toggleExtended()
         }
     
         toggleExtended() {
+            this.element.current.style.height = !this.state.extended ? `${this.originalHeight}px` : 0
             this.setState({ extended: !this.state.extended })
         }
     
         render() {
             return (
-                <WrappedComponent toggleExtended={() => this.toggleExtended()} extended={this.state.extended} {...this.props} />
+                <WrappedComponent toggleExtended={() => this.toggleExtended()} extended={this.state.extended} extendableElement={this.element} {...this.props} />
             )
         }
     }
