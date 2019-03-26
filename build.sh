6977074fbaceeedc51b36c30aa1acc491bf2bf73
Martin Dittmar <martin.dittmar@charite.de>
2019-03-26 15:01:39 +0100

    Datum zu Html-Titel hinzufügen

--- build.sh
+++ build.sh
@@ -10,8 +10,10 @@ fi
 
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo -e "  ---  Build für Verzeichnis \e[44m\e[97m $DIR \e[0m - Git-Branch \e[44m\e[97m $GIT_BRANCH \e[0m ---   "
-echo Ausführen von:    PUBLIC_URL="/$DIR" REACT_APP_SITE_TITLE="Branch $GIT_BRANCH, Deployment am $DATE" yarn build
 
 DATUM=`date '+%d.%m.%Y %H:%M'`
-PUBLIC_URL="/$DIR" REACT_APP_SITE_TITLE="Branch $GIT_BRANCH, Deployment am $DATE" yarn build
+echo Ausführen von:    PUBLIC_URL="/$DIR" REACT_APP_SITE_TITLE="Branch $GIT_BRANCH, Deployment am $DATUM" yarn build
+
+PUBLIC_URL="/$DIR" REACT_APP_SITE_TITLE="Branch $GIT_BRANCH, Deployment am $DATUM" yarn build
+
 cat htaccess-dist | sed s/RewriteBase/RewriteBase\ \\\/$DIR/g > build/.htaccess
