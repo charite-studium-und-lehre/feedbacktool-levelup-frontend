c4ae65aee63af2774b66495128e11fc766f8c703
Vincent <jan-vincent.wyszynski@charite.de>
2019-03-29 15:25:03 +0100

    fix unused import warning in Station component

--- src/Exams/Stations/Station.js
+++ src/Exams/Stations/Station.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import StationDetails from './StationDetails'
 import SimpleBar from '../../Charting/SimpleBar'
 import makeExtendable from '../../Core/makeExtendable'
