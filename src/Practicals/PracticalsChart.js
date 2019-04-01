7ad1c44cd3eca57abbafd7ed8bb575e70dbef370
Vincent <jan-vincent.wyszynski@charite.de>
2019-04-01 18:10:08 +0200

    change PracticalsChart to TimeChart

--- src/Practicals/PracticalsChart.js
+++ src/Practicals/PracticalsChart.js
@@ -2,27 +2,26 @@
 import React, {Component} from 'react';
 import _ from 'lodash'
 
-import { OrdinalChart } from '../Charting/Chart'
+import { TimeChart } from '../Charting/Chart'
 import LineGraph from '../Charting/LineGraph'
 import { XAxis, YAxis } from '../Charting/Axis'
 
-const createData = () => _.range(1,8).map(i => ({Semester: `Semester${i}`, tatig: _.random(0,6)}))
+const createData = () => _.range(1,8).map(i => ({Semester: new Date(2018 - i, 6 + _.random(-1, 2), 15 + _.random(-10, 20)), tatig: _.random(0,6)}))
 
 class PracticalsChart extends Component{
     constructor(props){
         super(props)
-        this.state = { data : [createData(), createData()] }
+        this.state = { data : createData() }
     }
 
-    render(){
-        let LineGraphs = this.state.data.filter((d,i) => this.props.graphs.indexOf(i) >= 0).map((d, i) => <LineGraph key={i} data={d.map(d => ({x: d.Semester, y: d.tatig}))} color="rgba(64,64,64,.3)"></LineGraph>)
+    render() {
         return (
             <div className="card p-3 h-100">
-                <OrdinalChart yDomain={[0,6]} xDomain={this.state.data[0].map(s => s.Semester)}>
+                <TimeChart yDomain={[0,6]} xDomain={[this.state.data[6].Semester, this.state.data[0].Semester]}>
                     <XAxis />
                     <YAxis ticks={{count: 3}} />
-                    {LineGraphs}
-                </OrdinalChart>
+                    <LineGraph data={this.state.data.map(d => ({x: d.Semester, y: d.tatig}))} color="rgba(64,64,64,.3)"></LineGraph>
+                </TimeChart>
             </div>
         )
     }
