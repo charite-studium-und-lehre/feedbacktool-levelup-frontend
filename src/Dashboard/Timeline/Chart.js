af6830d5cf3923763469fc70bb8191ff8115f21b
Vincent <jan-vincent.wyszynski@charite.de>
2019-04-04 19:44:25 +0200

    use linear interpolation of device width for number of years to display in Timeline

--- src/Dashboard/Timeline/Chart.js
+++ src/Dashboard/Timeline/Chart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { scaleLinear } from 'd3-scale'
 import { asChart, TimeScales } from '../../Charting/Chart'
 import { XAxis, YAxis } from '../../Charting/Axis'
 
@@ -7,7 +8,7 @@ const year = day * 365
 
 class Chart extends React.Component {
     componentDidMount() {
-        const years = this.props.width * 3 / 650 + 8 / 13
+        const years = scaleLinear().domain([300,950]).range([2,5])(this.props.width)
         !this.props.oldest && this.props.initTimerange(new Date(Date.now() - year * years))
     }
 
