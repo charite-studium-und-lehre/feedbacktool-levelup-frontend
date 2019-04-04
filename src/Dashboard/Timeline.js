1ceaf0f39ecc1de3e025b26d21fde66f22f38435
Vincent <jan-vincent.wyszynski@charite.de>
2019-04-04 13:01:38 +0200

    wip

--- src/Dashboard/Timeline.js
+++ src/Dashboard/Timeline.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { TimeChart } from '../Charting/Chart'
+import { TimeChart, asChart, TimeScales } from '../Charting/Chart'
 import PointGraph from '../Charting/PointGraph'
 import Legend from '../Charting/Legend'
 import { XAxis, YAxis } from '../Charting/Axis'
@@ -10,15 +10,23 @@ const LegendText = Legends.Dashboard.Timeline
 const day= 1000 * 60 * 60 * 24
 const year = day * 365
 
+const Chart = asChart(props => 
+    <TimeScales {...props} xDomain={[props.oldest || new Date(Date.now() - year * props.width / 100), props.newest]} yDomain={[0,100]}>
+        <YAxis label="% richtig" />
+        <XAxis ticks={{count: 5}} />
+        {props.children}
+    </TimeScales>
+)
+
 class Timeline extends Component {
     constructor(props) {
         super(props)
         this.state = {
             timerange: [new Date(Date.now() - year * 3), new Date()],
-            oldest: new Date(Date.now() - year * 3), 
+            oldest: null, 
             newest: new Date(), 
             selectedPoint: null 
-        }
+        }       
     }
 
     zoomIn(point, graph) {
@@ -44,17 +52,15 @@ class Timeline extends Component {
                 <div className="card-body">
                     <Legend title={LegendText.title}>{LegendText.text}</Legend>
                     <div className="p-3 pl-4 position-relative">
-                        <TimeChart xDomain={[this.state.oldest, this.state.newest]} yDomain={[0,100]}>
-                            <YAxis label="% richtig" />
-                            <XAxis ticks={{count: 5}} />
-                            {this.props.data.map((g, i) => (
-                                <PointGraph 
-                                    selectedPoint={this.state.selectedPoint ? this.state.selectedPoint.x : 0} 
-                                    onClick={ point => this.zoomIn(point, g) } 
-                                    key={i} data={g.data.map(d => ({ ...d, y: d.result }))} 
-                                    color={`hsla(${g.color}, 50%, 50%, .75)`} />
-                            ))}
-                        </TimeChart>
+                        <Chart oldest={this.state.oldest} newest={this.state.newest}>
+                        {this.props.data.map((g, i) => (
+                            <PointGraph 
+                                selectedPoint={this.state.selectedPoint ? this.state.selectedPoint.x : 0} 
+                                onClick={ point => this.zoomIn(point, g) } 
+                                key={i} data={g.data.map(d => ({ ...d, y: d.result }))} 
+                                color={`hsla(${g.color}, 50%, 50%, .75)`} />
+                        ))}
+                        </Chart>
                         <InfoOverlay 
                             visible={!!this.state.selectedPoint}
                             graph={this.state.graph || {}}
