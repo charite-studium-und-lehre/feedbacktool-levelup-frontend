78b6398ec7f81cf94b4bf4e8fa3552393525940f
towma <m.towma@hotmail.com>
2019-04-15 17:14:13 +0200

    Beratung Seite den EinfuerungText korrigieren

--- src/consulting/Consulting.js
+++ src/consulting/Consulting.js
@@ -16,7 +16,8 @@ export default function Consulting(props) {
                         </div>
                     </div>
                     <div className="col-lg-5 col-sm-6 consulting-text pt-5 pl-4 mb-5">
-                        <p>Im Laufe des Studiums wirst du sicherlich schon einige Situationen erlebt haben in denen du einen professionellen Rat zu den diversen Themen des Studienalltagsgebraucht hättest. Damit du immer gut informiert bist, wer dir in der jeweiligen Situation weiterhelfen kann, haben wir auf dieser Seite einige hilfreiche Beratungsangebote der Charitè zusammengefasst. Du erhältst hier eine Übersicht samt Links zu den Beratungsangeboten. Falls du dringend Unterstützung und einen vertrauensvollen Ansprechpartnerfür eventuelle Probleme im Studium benötigst, dann wende dich direkt per Mail an: 
+                        <p>Im Laufe des Studiums wirst du sicherlich schon einige Situationen erlebt haben in denen du einen professionellen Rat zu den diversen Themen des Studienalltags gebraucht hättest. Damit du immer gut informiert bist, wer dir in der jeweiligen Situation weiterhelfen kann, haben wir auf dieser Seite einige hilfreiche Beratungsangebote der Charitè zusammengefasst. Du erhältst hier eine Übersicht samt Links zu den Beratungsangeboten. Falls du dringend Unterstützung und einen vertrauensvollen Ansprechpartner für eventuelle Probleme im Studium benötigst, dann wende dich direkt per Mail an: 
+
                         <span> medicoach@charite.de</span>
                         </p>
                         <button type="button" className="btn btn-primary"><a href="https://www.charite.de/studium_lehre/service_beratung/">Mehr erfahren</a> </button>
