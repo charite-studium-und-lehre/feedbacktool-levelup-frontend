09d4487f1b8384ba92f5cdcc5f27ae5b7415b3e3
Martin Dittmar <martin.dittmar@charite.de>
2019-06-11 16:45:05 +0200

    Versuch, Seiten-Cache auszuschalten

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
+unregister()
