77a1103eeb561497f16e4ec3c90b24226fe4b939
Martin Dittmar <martin.dittmar@charite.de>
2019-06-11 18:15:55 +0200

    Update Zim-Doku Schnittstelle

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+// registerServiceWorker()
 unregister()
