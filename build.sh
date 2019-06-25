27232844d08c4c88bd9132b93ec25ef37ebd8d65
Vincent <jan-vincent.wyszynski@charite.de>
2019-06-25 15:57:13 +0200

    add yarn install to build.sh

--- build.sh
+++ build.sh
@@ -12,8 +12,9 @@ GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo -e "  ---  Build für Verzeichnis \e[44m\e[97m $DIR \e[0m - Git-Branch \e[44m\e[97m $GIT_BRANCH \e[0m ---   "
 
 DATUM=`date '+%d.%m.%Y %H:%M'`
-echo Ausführen von:    PUBLIC_URL="/$DIR" REACT_APP_SITE_TITLE="Branch $GIT_BRANCH, Deployment am $DATUM" yarn build
+yarn install
 
+echo Ausführen von:    PUBLIC_URL="/$DIR" REACT_APP_SITE_TITLE="Branch $GIT_BRANCH, Deployment am $DATUM" yarn build
 PUBLIC_URL="/$DIR" REACT_APP_SITE_TITLE="Branch $GIT_BRANCH, Deployment am $DATUM" yarn build
 
 cat htaccess-dist | sed s/RewriteBase/RewriteBase\ \\\/$DIR/g > build/.htaccess
