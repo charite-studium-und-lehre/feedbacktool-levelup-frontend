fb44fffdbac653d27983c084b3c3b938304b76e5
Vincent <jan-vincent.wyszynski@charite.de>
2019-06-25 15:16:15 +0200

    revert unregistering service worker

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-// import registerServiceWorker from './registerServiceWorker'
-import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'))
-// registerServiceWorker()
-unregister()
+registerServiceWorker()
