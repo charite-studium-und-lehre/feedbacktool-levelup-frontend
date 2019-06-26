add8c9071dfbf61178d189748d5ac2f85d18b3a0
toma <mohammed.alhassan-altoaama@charite.de>
2019-06-26 13:56:48 +0200

    "Habe ich gemacht" und "Traue misch zu" anders  platzieren

--- src/Practicals/Container.js
+++ src/Practicals/Container.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Item from './Item'
-import Score from './Score'
 import Legend from '../Charting/Legend'
 import Legends from '../Core/LegendTexts'
 import Toolbar from './Toolbar'
@@ -14,30 +13,24 @@ class Container extends React.Component {
         this.state = { edit: false }
     }
 
-    toggleEdit = () => this.setState({edit: !this.state.edit})
+    toggleEdit = () => this.setState({ edit: !this.state.edit })
 
     render() {
-        const {t} = this.props
+        const { t } = this.props
         return (
-        <div style={{fontSize: '.9rem'}}>
-            <div className="card p-2">
-                <Legend title={Legends(t).Practicals.Main.title}>{Legends(t).Practicals.Main.text}</Legend>
-                <div className="row">
-                    <div className="col-12">
-                        <Score headings={true} entry={this.props.root} />
-                    </div>
+            <div style={{ fontSize: '.9rem' }}>
+                <div className="card p-2">
+                    <Legend title={Legends(t).Practicals.Main.title}>{Legends(t).Practicals.Main.text}</Legend>
                 </div>
-            </div>
-            <Toolbar toggleEdit={this.toggleEdit} edit={this.state.edit} />
-            <div className="row">
-                {this.props.root.entries.map(e =>
-                    <div key={e} className="col-12">
-                        <Item edit={this.state.edit} entryId={e} level={1} />
-                    </div>
-                )}
-            </div>
-        </div>)
+                <Toolbar toggleEdit={this.toggleEdit} edit={this.state.edit} />
+                <div className="row">
+                    {this.props.root.entries.map(e =>
+                        <div key={e} className="col-12">
+                            <Item edit={this.state.edit} entryId={e} level={1} />
+                        </div>
+                    )}
+                </div>
+            </div>)
     }
 }
-// var a = selectors.getItemByLabel(state, 'root')
-export default connect(state => ({ root: selectors.getItemByLabel(state, 'root') }))( withTranslation()(Container))
+export default connect(state => ({ root: selectors.getItemByLabel(state, 'root') }))(withTranslation()(Container))
--- src/Practicals/Toolbar.js
+++ src/Practicals/Toolbar.js
@@ -4,9 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faFileExport, faEnvelopeOpenText, faListOl } from '@fortawesome/free-solid-svg-icons'
 import makeExtendable from '../Core/makeExtendable'
 import List from './List'
+import Score from './Score'
 import { withTranslation } from 'react-i18next'
 
-const Button = (props, {t}) => 
+const Button = (props) =>
     <div className={`d-inline-block ${props.className || ''}`}>
         <button className={`btn btn-sm mr-2 ${props.active ? 'btn-success' : 'btn-primary'}`} onClick={props.onClick}>
             <span className="d-none d-lg-inline mr-2">{props.children}</span>
@@ -18,40 +19,47 @@ class Toolbar extends React.Component {
     constructor(props) {
         super(props)
         this.state = { list: false }
-     }
-     
-     render () {
-         const {t} = this.props
-        return (
-        <div className="p-2 mt-2 card text-right sticky-top" style={{top: '3.7rem'}}>
-            <div>
-                <Button className="d-lg-none" icon={faListOl} active={this.state.list} onClick={() => this.setState({ list: !this.state.list })} />
-                <Button icon={faFileExport}>Export</Button>
-                <Button icon={faEnvelopeOpenText} active={this.props.extended} onClick={this.props.toggleExtended}>{t(`Fremdeinschätzung</B`)}</Button>
-                <Button icon={faEdit} active={this.props.edit} onClick={this.props.toggleEdit}>{this.props.edit ? 'speichern' : 'bearbeiten'}</Button>
-                <SlideDown className="animated fast">
-                    {this.state.list &&
-                    <div className="text-left bg-white px-2">
-                        <List />
-                    </div>}
-                </SlideDown>
-                <SlideDown className="animated fast">
-                    {this.props.extended && 
-                    <div className="p-2">
-                        <div className="d-flex">
-                            <div className="flex-grow-1">
-                                <input className="form-control form-control-sm" placeholder="email"></input>
-                            </div>
-                            <div className="">
-                                <button className="btn btn-sm btn-success" onClick={this.props.toggleExtended}>senden</button>
-                            </div>
-                        </div>
-                        <div className="text-secondary text-left" style={{fontSize: '.7rem'}}>{t(`Wir senden einen Link an diese Email-Adresse, über den eine Fremdeinschätzung abgegeben werden kann.`)}</div>
-                    </div>}
-                </SlideDown>
-            </div>
-        </div>
-        )}
     }
+    render() {
+        const { t } = this.props
+        return (
+            <div className="p-2 mt-2 card text-right sticky-top" style={{ top: '3.7rem' }}>
+                <div>
+                    <div className="row">
+                        <div className="col-7">
+                            <Score headings={true} entry={this.props.root} />
+                        </div>
+                        <div className="col-5">
+                            <Button className="d-lg-none" icon={faListOl} active={this.state.list} onClick={() => this.setState({ list: !this.state.list })} />
+                            <Button icon={faFileExport}>{t(`Export`)}</Button>
+                            <Button icon={faEnvelopeOpenText} active={this.props.extended} onClick={this.props.toggleExtended}>{t(`Fremdeinschätzung</B`)}</Button>
+                            <Button icon={faEdit} active={this.props.edit} onClick={this.props.toggleEdit}>{this.props.edit ? t(`speichern`): t(`bearbeiten`)}</Button>
+                        </div>
+                    </div>
+                    <SlideDown className="animated fast">
+                        {this.state.list &&
+                            <div className="text-left bg-white px-2">
+                                <List />
+                            </div>}
+                    </SlideDown>
+                    <SlideDown className="animated fast">
+                        {this.props.extended &&
+                            <div className="p-2">
+                                <div className="d-flex">
+                                    <div className="flex-grow-1">
+                                        <input className="form-control form-control-sm" placeholder="email"></input>
+                                    </div>
+                                    <div className="">
+                                        <button className="btn btn-sm btn-success" onClick={this.props.toggleExtended}>senden</button>
+                                    </div>
+                                </div>
+                                <div className="text-secondary text-left" style={{ fontSize: '.7rem' }}>{t(`Wir senden einen Link an diese Email-Adresse, über den eine Fremdeinschätzung abgegeben werden kann.`)}</div>
+                            </div>}
+                    </SlideDown>
+                </div>
+            </div>
+        )
+    }
+}
 
-export default withTranslation() (makeExtendable(Toolbar))
+export default withTranslation()(makeExtendable(Toolbar))
