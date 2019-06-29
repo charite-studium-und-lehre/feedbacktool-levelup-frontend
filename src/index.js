f833f1e1f14293089fe14120425f54731556303b
Vincent <jan-vincent.wyszynski@charite.de>
2019-06-29 18:24:39 +0200

    update service worker stuff to newer create-react-app version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import './i18n'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+serviceWorker.register()
