f4bba14edb17fc186eba69b7ab3090b19e185cec
Vincent Wyszynsky <vincent@s-mfal-feed-ext.charite.de>
2019-06-30 01:48:23 +0200

    fiddle with service worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import './i18n'
 import App from './App'
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(<App />, document.getElementById('root'))
-serviceWorker.register()
+serviceWorker.register({
+	onUpdate: registration => {
+		const waitingServiceWorker = registration.waiting
+
+		if (waitingServiceWorker) {
+			waitingServiceWorker.addEventListener("statechange", event => {
+            			if (event.target.state === "activated") {
+					window.location.reload()
+            			}
+        		})
+        		waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+      		}		
+    	}
+})
