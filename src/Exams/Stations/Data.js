ddf683d4e3101982920efcadf9e7c3fa314b6fae
toma <mohammed.alhassan-altoaama@charite.de>
2019-07-02 16:45:24 +0200

    text korrigieren

--- src/Exams/Stations/Data.js
+++ src/Exams/Stations/Data.js
@@ -100,7 +100,7 @@ const data = [
             },
             {
                 name: 'Anatomie / Biologie', 
-                category: 'theoretische', 
+                category: 'Theoretisch', 
                 details: [
                     {
                         label: 'Thema',
@@ -223,7 +223,7 @@ const data = [
         stations: [
             {
                 name: 'Physiologie 1', 
-                category: 'theoretische', 
+                category: 'Theoretisch', 
                 details: [
                     {
                         label: 'Thema',
@@ -243,7 +243,7 @@ const data = [
             },
             {
                 name: 'Physiologie 2', 
-                category: 'theoretische', 
+                category: 'Theoretisch', 
                 details: [
                     {
                         label: 'Thema',
@@ -263,7 +263,7 @@ const data = [
             },
             {
                 name: 'Anatomie', 
-                category: 'theoretische', 
+                category: 'Theoretisch', 
                 details: [
                     {
                         label: 'Thema',
@@ -283,7 +283,7 @@ const data = [
             },
             {
                 name: 'Anatomie 2', 
-                category: 'theoretische', 
+                category: 'Theoretisch', 
                 details: [
                     {
                         label: 'Thema',
