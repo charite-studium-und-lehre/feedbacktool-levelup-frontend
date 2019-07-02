6e9a1f775e7719d39ef931487dc9c6732942267f
Vincent <jan-vincent.wyszynski@charite.de>
2019-07-02 15:52:21 +0200

    move ptm subject chart colours to variable

--- src/Exams/Ptm/SubjectAlt.js
+++ src/Exams/Ptm/SubjectAlt.js
@@ -1,7 +1,7 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
+import tinycolor from 'tinycolor2'
 import { OrdinalChart } from '../../Charting/Chart'
 import AreaGraph from '../../Charting/AreaGraph'
 import LineGraph from '../../Charting/LineGraph'
@@ -10,6 +10,7 @@ import { selectors } from './Store'
 import SimpleBar from '../../Charting/SimpleBar'
 import Legende from './Legende'
 
+const colors = ["hsla(120, 50%, 50%, .4)", "hsla(0, 50%, 50%, .4)"]
 const SubjectAlt = props => (
     <div className="card m-2 flex-grow-1" style={{width: '20rem'}}>
         <div className="card-body">
@@ -19,16 +20,16 @@ const SubjectAlt = props => (
                     {props.data.richtig} von {props.data.gesamt} richtig
                 </SimpleBar>
             </div>
-            <Legende text='gestellte Anzahl Fragen' style={{backgroundColor : '#B3E5B3'}}/>
-            <Legende text='richtige Anzahl Fragen'  style={{backgroundColor : '#B8A385'}}/>
+            <Legende text='Anzahl gestellte Fragen' style={{backgroundColor : colors[0]}}/>
+            <Legende text='Anzahl richtige Fragen'  style={{backgroundColor : colors[1]}}/>
             <div className="p-4 mt-3 mb-4" style={{height: '6rem'}} >
                 <OrdinalChart xDomain={props.timeline.map(d => d.semester)} yDomain={[0, Math.max(...props.timeline.map(d => d.gesamt))]}>
                     <XAxis />
-                    <YAxis label="# Fragen" ticks={{count: 3}} />
-                    <AreaGraph data={props.timeline.map(d => ({ x: d.semester, y0: 0, y1: d.gesamt }))} color="hsla(120, 50%, 50%, .4)" />
-                    <LineGraph data={props.timeline.map(d => ({ x: d.semester, y: d.gesamt }))} color="hsla(120, 50%, 20%, .4)" />
-                    <AreaGraph data={props.timeline.map(d => ({ x: d.semester, y0: 0, y1: d.richtig }))} color="hsla(0, 50%, 50%, .4)" />
-                    <LineGraph data={props.timeline.map(d => ({ x: d.semester, y: d.richtig }))} color="hsla(0, 50%, 20%, .4)" />
+                    <YAxis ticks={{count: 3}} />
+                    <AreaGraph data={props.timeline.map(d => ({ x: d.semester, y0: 0, y1: d.gesamt }))} color={colors[0]} />
+                    <LineGraph data={props.timeline.map(d => ({ x: d.semester, y: d.gesamt }))} color={tinycolor(colors[0]).darken(30).toString()} />
+                    <AreaGraph data={props.timeline.map(d => ({ x: d.semester, y0: 0, y1: d.richtig }))} color={colors[1]} />
+                    <LineGraph data={props.timeline.map(d => ({ x: d.semester, y: d.richtig }))} color={tinycolor(colors[1]).darken(30).toString()} />
                 </OrdinalChart>
             </div>
            
