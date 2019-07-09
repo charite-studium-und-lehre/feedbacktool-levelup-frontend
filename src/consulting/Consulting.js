7c07d2633021df30c121f827b78722e69f640c40
toma <mohammed.alhassan-altoaama@charite.de>
2019-07-09 12:29:36 +0200

    fix text BereatungSeite

--- src/consulting/Consulting.js
+++ src/consulting/Consulting.js
@@ -15,7 +15,7 @@ import { ConsultingTree } from './ConsultingTree'
                         </div>
                     </div>
                     <div className="col-lg-5 col-sm-6 consulting-text pt-5 pl-4 mb-5">
-                        <p>{t(`Im Laufe des Studiums wirst du sicherlich schon einige Situationen erlebt haben in denen du einen professionellen Rat zu den diversen Themen des Studienalltags gebraucht hättest. Damit du immer gut informiert bist, wer dir in der jeweiligen Situation weiterhelfen kann, haben wir auf dieser Seite einige hilfreiche Beratungsangebote der Charitè zusammengefasst. Du erhältst hier eine Übersicht samt Links zu den Beratungsangeboten. Falls du dringend Unterstützung und einen vertrauensvollen Ansprechpartner für eventuelle Probleme im Studium benötigst, dann wende dich direkt per Mail an: `)}
+                        <p>{t(`Im Laufe des Studiums wirst du sicherlich schon einige Situationen erlebt haben in denen du einen professionellen Rat zu den diversen Themen des Studienalltags gebraucht hättest. Damit du immer gut informiert bist, wer dir in der jeweiligen Situation weiterhelfen kann, haben wir auf dieser Seite einige hilfreiche Beratungsangebote der Charitè zusammengefasst. Du erhältst hier eine Übersicht samt Links zu den Beratungsangeboten. Falls du dringend Unterstützung und einen vertrauensvollen Ansprechpartner für eventuelle Probleme im Studium benötigst, dann wende dich direkt per Mail an`)}:
 
                         <span> {t(`medicoach@charite.de`)}</span>
                         </p>
