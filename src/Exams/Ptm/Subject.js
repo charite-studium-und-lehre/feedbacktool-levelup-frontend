d9ee530777c33a95bc2d72b168930b2d5835d11d
vincent <vincent@whoop.com>
2019-07-27 00:43:23 +0200

    try new ptm subjects layout

--- src/Exams/Ptm/Subject.js
+++ src/Exams/Ptm/Subject.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
 import tinycolor from 'tinycolor2'
+import makeExtendable from '../../Core/makeExtendable'
 import { OrdinalChart } from '../../Charting/Chart'
 import AreaGraph from '../../Charting/AreaGraph'
 import LineGraph from '../../Charting/LineGraph'
@@ -10,29 +11,35 @@ import { selectors } from './Store'
 import SimpleBar from '../../Charting/SimpleBar'
 import Legende from './Legende'
 
+const Timeline = withTranslation()(({ t, data }) => 
+    <div>
+        <Legende text={t('Anzahl gestellte Fragen')} style={{backgroundColor : colors[0]}}/>
+        <Legende text={t('Anzahl richtige Fragen')} style={{backgroundColor : colors[1]}}/>
+        <div className="p-4 mt-3" style={{height: '6rem'}} >
+            <OrdinalChart xDomain={data.map(d => d.short)} yDomain={[0, Math.max(...data.map(d => d.gesamt))]}>
+                <XAxis />
+                <YAxis ticks={{count: 3}} />
+                <AreaGraph data={data.map(d => ({ x: d.short, y0: d.richtig, y1: d.gesamt }))} color={colors[0]} />
+                <LineGraph data={data.map(d => ({ x: d.short, y: d.gesamt }))} color={tinycolor(colors[0]).darken(30).toString()} />
+                <AreaGraph data={data.map(d => ({ x: d.short, y0: 0, y1: d.richtig }))} color={colors[1]} />
+                <LineGraph data={data.map(d => ({ x: d.short, y: d.richtig }))} color={tinycolor(colors[1]).darken(30).toString()} />
+            </OrdinalChart>
+        </div>
+    </div>)
+
 const colors = ["hsla(120, 50%, 50%, .4)", "hsla(0, 50%, 50%, .4)"]
-const SubjectAlt = props => (
-    <div className="card m-2 flex-grow-1" style={{width: '20rem'}}>
+const Subject = props => (
+    <div className="card m-2">
         <div className="card-body">
-            <span className="font-weight-bold">{props.name}</span>
+            <h5><span className="text-primary">#{props.rank}</span><span className="ml-1 font-weight-bold">{props.name}</span></h5>
+            
             <div className="my-3">
                 <SimpleBar value={props.data.richtig} total={props.data.gesamt}>
                     {props.data.richtig} von {props.data.gesamt} richtig
                 </SimpleBar>
             </div>
-            <Legende text='Anzahl gestellte Fragen' style={{backgroundColor : colors[0]}}/>
-            <Legende text='Anzahl richtige Fragen'  style={{backgroundColor : colors[1]}}/>
-            <div className="p-4 mt-3 mb-4" style={{height: '6rem'}} >
-                <OrdinalChart xDomain={props.timeline.map(d => d.short)} yDomain={[0, Math.max(...props.timeline.map(d => d.gesamt))]}>
-                    <XAxis />
-                    <YAxis ticks={{count: 3}} />
-                    <AreaGraph data={props.timeline.map(d => ({ x: d.short, y0: d.richtig, y1: d.gesamt }))} color={colors[0]} />
-                    <LineGraph data={props.timeline.map(d => ({ x: d.short, y: d.gesamt }))} color={tinycolor(colors[0]).darken(30).toString()} />
-                    <AreaGraph data={props.timeline.map(d => ({ x: d.short, y0: 0, y1: d.richtig }))} color={colors[1]} />
-                    <LineGraph data={props.timeline.map(d => ({ x: d.short, y: d.richtig }))} color={tinycolor(colors[1]).darken(30).toString()} />
-                </OrdinalChart>
-            </div>
-           
+            <div className="text-primary" onClick={() => props.toggleExtended()}><a className="float-right">Entwicklung</a></div>
+            {props.extended && <Timeline data={props.timeline} />}
           {/* <div>
           <Link to={`/strengths/${(props.name)}`} className="mt-2 float-right text-primary" style={{fontSize:".8rem", textDecoration:"underline"}}>{props.t(`zu deiner Entwicklung in diesem Fach`)}</Link>
           </div> */}
@@ -45,4 +52,4 @@ const stateToProps = (state, ownProps) => ({
     timeline: selectors.getAllForSubject(state, ownProps.name)
 })
 
-export default withTranslation() (connect(stateToProps)(SubjectAlt))
+export default makeExtendable(connect(stateToProps)(Subject))
--- src/Exams/Ptm/Subjects.js
+++ src/Exams/Ptm/Subjects.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
+import { withTranslation } from 'react-i18next'
 import _ from 'lodash/fp'
-import SubjectsTabs from '../../Core/Tabs'
 import needsData from '../../Core/needsData'
+import Filter from '../../Utils/Filter'
 import Subject from './Subject'
 import { selectors, actions } from './Store'
+import css from './Subjects.module.css'
 
-const stateToProps = (state, { semester }) => selectors.getBySemester(state, semester)
-const Subjects = _.compose(needsData(selectors.loaded, actions.load), connect(stateToProps))( ({ fächer, semester }) => 
-    <SubjectsTabs>
-        {_.flow(_.groupBy(f => f.kategorie), _.map(c => 
-            <div key={c[0].kategorie} title={c[0].kategorie} className="d-flex flex-wrap">
-                {_.sortBy(s => s.title, c).map(s => <Subject key={s.name} name={s.name} semester={semester} />)}
+const stateToProps = (state, { semester }) => _.flow(selectors.getBySemester, selectors.getSubjects, _.sortBy( s => -s.richtig / s.gesamt ), r => ({ fächer: r}))(state, semester)
+const Subjects = _.compose(needsData(selectors.loaded, actions.load), withTranslation(), connect(stateToProps))( ({ t, fächer, semester }) => {
+    const categories = _.keys(_.groupBy( s => s.kategorie, fächer ))
+    const [ categoryFilters, setCategoryFilters ] = useState( categories.map(c => ({label: c, pred: d => d.kategorie === c , selected: true, color: 'hsla(200, 50%, 50%, .8)' })) )
+    const [ search, setSearch ] = useState('')
+
+    const filtered = fächer.filter(
+        _.overEvery([
+            _.overSome(categoryFilters.filter( f => f.selected ).map( f => f.pred )), 
+            s => s.name.toLowerCase().indexOf(search.toLowerCase()) >= 0
+        ]))
+    return (
+    <div className="w-100">
+        <div className="d-flex">
+            {/* {t('Filter')}:  */}
+            <div className="">
+                <Filter
+                    filters={ categoryFilters } 
+                    onUpdate={ setCategoryFilters } />
             </div>
-        ))(fächer)}
-    </SubjectsTabs>
-)
+            <div className="flex-grow-1 align-middle py-2">
+                <input type="text" placeholder="suchen..." onKeyUp={e => setSearch(e.target.value)} className={`p-1 ${css.input}`}></input>
+            </div>
+        </div>
+        {filtered.map((s, rank) =>
+            <div className="w-100" key={s.name}>
+                <Subject key={s.name} name={s.name} rank={parseInt(rank) + 1} semester={semester} />
+            </div>
+        )}
+    </div>)
+})
+
 export default Subjects
