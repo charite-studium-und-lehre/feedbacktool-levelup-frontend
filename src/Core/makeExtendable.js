27e77c3a607e416551b192aea5490c2b9ce7f51f
vincent <vincent@whoop.com>
2019-07-26 16:08:03 +0200

    fix makeExtenablde to respect if extended props is set

--- src/Core/makeExtendable.js
+++ src/Core/makeExtendable.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const makeExtendable = (WrappedComponent, def = false) => 
     props => {
-        const [ extended, setExtended ] = useState( def && (!def || window.innerWidth > 768) )
+        const [ extended, setExtended ] = useState( props.extended || (def && (!def || window.innerWidth > 768)) )
     
         return <WrappedComponent toggleExtended={() => setExtended(!extended)} {...props} extended={extended} />
     }
