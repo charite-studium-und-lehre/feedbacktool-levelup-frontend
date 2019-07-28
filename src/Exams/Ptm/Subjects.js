5898baf85edd30d60b8c873fa80f534a34ce7e89
vincent <vincent@whoop.com>
2019-07-28 16:32:49 +0200

    clean up filters in subjects

--- src/Exams/Ptm/Subjects.js
+++ src/Exams/Ptm/Subjects.js
@@ -12,23 +12,24 @@ import { minQuestions } from '../../Utils/Constants';
 const stateToProps = (state, { semester }) => _.flow(selectors.getBySemester, selectors.getSubjects, _.sortBy( s => -s.richtig / s.gesamt ), r => ({ fächer: r}))(state, semester)
 const Subjects = _.compose(needsData(selectors.loaded, actions.load), withTranslation(), connect(stateToProps))( ({ t, fächer, semester }) => {
     const categories = _.keys(_.groupBy( s => s.kategorie, fächer ))
-    const [ categoryFilters, setCategoryFilters ] = useState( categories.map(c => ({label: c, pred: d => d.kategorie === c , selected: true, color: 'hsla(200, 50%, 50%, .8)' })) )
-    const [ minQuestionsFilter, setMinQuestionsFilter ] = useState([{label: `mind. ${minQuestions} Fragen`, pred: s => s.gesamt >= minQuestions , selected: true, color: 'hsla(200, 50%, 50%, .8)' }])
+    const [ filters, setFilters ] = useState( 
+        categories.map(c => ({label: c, pred: d => d.kategorie === c , selected: true, color: 'hsla(200, 50%, 50%, .8)', group: 'categories'}))
+        .concat({label: `mind. ${minQuestions} Fragen`, pred: s => s.gesamt >= minQuestions , selected: true, color: 'hsla(200, 50%, 50%, .8)' })
+    )
     const [ search, setSearch ] = useState('')
 
     const filtered = fächer.filter(
         _.overEvery([
-            _.overSome(categoryFilters.filter( f => f.selected ).map( f => f.pred )), 
+            _.overSome(filters.filter( f => f.selected && f.group === 'categories').map( f => f.pred )), 
             s => s.name.toLowerCase().indexOf(search.toLowerCase()) >= 0,
-            _.overEvery([...minQuestionsFilter.filter( f => f.selected ).map( f => f.pred ), () => true]),
+            _.overEvery([...filters.filter( f => f.selected && f.group !== 'categories' ).map( f => f.pred ), () => true]),
         ]))
     return (
     <div className="w-100">
         <div className="d-flex flex-column flex-lg-row">
             {/* {t('Filter')}:  */}
             <div className="">
-                <Filter filters={ categoryFilters } onUpdate={ setCategoryFilters } />
-                <Filter filters={ minQuestionsFilter } onUpdate={ setMinQuestionsFilter } />
+                <Filter filters={ filters } onUpdate={ setFilters } />
             </div>
             <div className="flex-grow-1 align-middle py-2">
                 <input type="text" placeholder="suchen..." onKeyUp={e => setSearch(e.target.value)} className={`p-1 ${css.input}`}></input>
