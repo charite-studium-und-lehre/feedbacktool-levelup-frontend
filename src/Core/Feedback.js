62825186a81f80c5613b594acf543685334f3e29
Vincent <jan-vincent.wyszynski@charite.de>
2019-07-31 13:42:31 +0200

    polish feedback visuals send feedback on enter pressed

--- src/Core/Feedback.js
+++ src/Core/Feedback.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react'
 import socketio from "socket.io-client"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
+import { faTimesCircle, faPaperPlane } from '@fortawesome/free-regular-svg-icons'
 
 const url = 'https://lu-feedback.herokuapp.com/'
 
@@ -11,13 +11,18 @@ const style = {
     position: 'absolute',
     bottom: 0,
     right: 0,
+    fontSize: '.8rem',
+    zIndex: 100,
 }
 const Feedback = () => {
     const input = useRef(null)
     const messageDiv = useRef(null)
     const [ ts, setTs ] = useState(null)
-    const [ show, setShow ] = useState(false)
-    const [ messages, setMessages ] = useState([])
+    const [ show, setShow ] = useState(true)
+    const [ messages, setMessages ] = useState([{
+        text: 'Das hier ist LevelUp! Wir wollen dir auf dieser Plattform in Zukunft Rückmeldung zu deinem Fortschritt im Studium geben. Daher würden wir gerne deine Meinung dazu hören, was wir noch verbessern können. Schreib uns hier im Textfeld einfach alles, was dir einfällt. Bitte beachte, dass LevelUp noch nicht fertig ist und es noch ein paar Fehler geben kann. Außerdem werden hier nur Beispieldaten angezeigt, die nichts mit deinen echten Ergebnissen zu tun haben.',
+        sender: 'server'
+    }])
 
     function pushMessage(message) {
         setMessages(messages.concat(message))
@@ -49,20 +54,24 @@ const Feedback = () => {
         input.current.value = ''
     }
 
+    function handleKeyPress(e) {
+        if(e.keyCode === 13) postFeedback()
+    }
+
     return <div style={style} className="p-4">
-        { show && <div style={{width: '18rem', backgroundColor: 'lightyellow', border: '1px solid grey'}}>
-            <div className="text-right px-2" onClick={() => setShow(false)} style={{backgroundColor: 'lightgrey'}}>
-                <FontAwesomeIcon className="text-white" icon={faTimesCircle} />
+        { show && <div className="with-shadow" style={{width: '18rem', backgroundColor: 'white', border: 'none'}}>
+            <div className="text-right px-2 bg-primary" style={{height: '2rem', lineHeight: '2.1rem'}} onClick={() => setShow(false)} >
+                <FontAwesomeIcon style={{fontSize: '1rem'}} className="text-white" icon={faTimesCircle} />
             </div>
             <div ref={messageDiv} className="p-2" style={{maxHeight: '17rem', overflow: 'scroll'}}>
-                {messages.map((msg, i) => <p style={{borderRadius: '.3rem'}} className={`p-1 text-white ${msg.sender === 'server' ? 'bg-success' : 'bg-info'}`} key={i}>{msg.text}</p>)}
+                {messages.map((msg, i) => <div style={{borderRadius: '.3rem'}} className={`d-inline-block mt-2 p-2 text-white ${msg.sender === 'server' ? 'bg-info ml-3' : 'bg-secondary mr-3'}`} key={i}>{msg.text}</div>)}
             </div>
-            <div className="text-right p-2">
-                <input className="w-100" placeholder="Dein Feedback..." ref={input}></input
-                ><button className="btn btn-primary mt-1"  onClick={ () => postFeedback() }>senden</button>
+            <div className="text-right p-2 d-flex">
+                <input style={{border: 'none', borderBottom: '1px solid lightgrey'}} className="flex-grow-1 no-outline" placeholder="Dein Feedback..." ref={input} onKeyUp={e => handleKeyPress(e)}></input>
+                <FontAwesomeIcon className="text-primary m-2" style={{fontSize: '1rem'}} icon={faPaperPlane} onClick={ () => postFeedback() } />
             </div>
         </div>}
-        {!show && <div className="w-100 text-right">
+        {!show && <div className="w-100 text-right with-shadow">
             <button className="btn btn-success" onClick={ () => setShow(true) }>feedback</button>
         </div>}
     </div>
