ef623e10e9bd8c4bde26765106c903bff9d8ae93
Vincent <jan-vincent.wyszynski@charite.de>
2019-08-21 01:44:50 +0200

    add summing of progress items

--- src/Dashboard/Progress/Store.js
+++ src/Dashboard/Progress/Store.js
@@ -6,8 +6,17 @@ import BaseStore from '../../Core/BaseStore'
 export const identifier = 'progress'
 const baseStore = BaseStore(identifier)
 
+const traverse = p => e => e.entries ? _.sum(e.entries.map(traverse(p))) : p(e)
+const getTotal = _.sumBy(traverse( () => 1 ))
+const getDone = _.sumBy(traverse( e => e.done ))
+const dashboardData = _.flow([
+    _.over([ getTotal, getDone ]),
+    ([ total, done ]) => ({ total, done })
+])
+
 export const selectors = baseStore.withLoadedSelector({
     getTree: state => baseStore.getItems(state),
+    getDashboardData: _.flow([ baseStore.getItems, dashboardData ])
 })
 
 export const actions = baseStore.withLoadAction({}, Results)
