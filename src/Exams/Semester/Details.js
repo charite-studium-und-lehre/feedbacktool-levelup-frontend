b7c5e2ad6167e28b7f57792a4a02697a4d8b07fc
toma <mohammed.alhassan-altoaama@charite.de>
2019-09-30 15:29:04 +0200

    McFragen Card

--- src/Exams/Semester/Details.js
+++ src/Exams/Semester/Details.js
@@ -7,6 +7,7 @@ import Legend from '../../Charting/Legend'
 import SimpleDot from '../../Charting/SimpleDot'
 import Legends from '../../Core/LegendTexts'
 import needsData from '../../Core/needsData'
+import McFargen from './McFragen'
 import BarWithHeader from './BarWithHeader'
 import { selectors, actions } from './Store'
 
@@ -35,25 +36,20 @@ fächer.map(d =>
 })
 
 const Details = withTranslation()(({ t, id }) => {
-    const [ mode, setMode ] = useState('modules')
-    const LegendText = Legends.Exams.Semester.Details
+    const [mode, setMode] = useState('modules')
+    const LegendText = Legends.Exams.Semester
     return (
-        <div>
-            <div className="card p-4" style={{ overflow: 'hidden' }}>
-                <Legend title={LegendText.title}>
-                    {LegendText.text}
+        <div className='row '>
+            <div className=" col-lg-8 p-1" style={{ overflow: 'hidden' }}>
+               <div className='card p-3'>
+               <Legend title={LegendText.Details.title}>
+                    {LegendText.Details.text}
                     <div className="position-relative">
                         Der <SimpleDot style={{ position: 'relative', display: 'inline-block', marginLeft: '.75rem' }} value={0} />  {t(`kennzeichnet den Kohortenmittelwert.`)}
-                </div>
-                </Legend>
-
-                <div className="row" >
-                    <div className="mt-3 col-md-5">
-                        <Link to='1.%20Fachsemester/questions'>
-                            <button type="button" className="btn btn-outline-primary">Fragen und Antworten</button>
-                        </Link>
                     </div>
-                    <div className="col-md-6 mt-4">
+                </Legend>
+                <div >
+                    <div className="mt-4">
                         <label className="m-0 mr-2"><input type="radio" name="details.mode" checked={mode === 'modules'} onChange={() => setMode('modules')} className="mx-2" />Module</label>
                         <label><input type="radio" name="details.mode" checked={mode === 'subjects'} onChange={() => setMode('subjects')} className="mx-2" />Fächer</label>
                     </div>
@@ -61,6 +57,18 @@ const Details = withTranslation()(({ t, id }) => {
                 <div className="mt-3">
                     <Chart mode={mode} id={id} />
                 </div>
+               </div>
+            </div>
+            <div className="col-lg-4 p-1">
+                <div className='card p-3'>
+                <Legend title={LegendText.McFargen.title}>
+                    {LegendText.McFargen.text}
+                </Legend>
+                <div>
+                  <McFargen/>
+                </div>
+                </div>
+               
             </div>
         </div>
     )
