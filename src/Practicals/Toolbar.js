f8c4e51a72b528986c40f40c9800ac76b3a01f3e
toma <mohammed.alhassan-altoaama@charite.de>
2019-10-07 17:26:20 +0200

    Level der Eigenständigkeit List bearbeiten

--- src/Practicals/Toolbar.js
+++ src/Practicals/Toolbar.js
@@ -17,34 +17,35 @@ const Button = (props) =>
     </div>
 
 const Toolbar = ({ t, ...props }) => {
-    const [list, setList] = useState(false)
     const [extended, setExtended] = useState({
         extended1:false,
-        extended2:false
+        extended2:false,
+        extended3:false
+
     })
     return (
         <div className="pt-2 pb-2  card" style={{ top: '1rem' }}>
             <div className='row'>
                 <div className="col-xs-12 col-sm-6 col-md-5 col-lg-5 pb-xs-5 ">
                     <div className="ml-2">
-                        <Button className="d-lg-none" icon={faListOl} active={list} onClick={() => setList(!list)} >{t(`Level der Eigenständigkeit`)}</Button>
-                        <Button icon={faEnvelopeOpenText} active={extended.extended1} onClick={()=> setExtended({extended1: !extended.extended1, extended2:false})}>{t(`Erhaltene Fremdeinschätzung `)}</Button>
-                        <Button icon={faEnvelopeOpenText} active={extended.extended2} onClick={()=> setExtended({extended1: false, extended2: !extended.extended2})}>{t(`Fremdeinschätzung einfordern`)}</Button>
+                        <Button className="d-lg-none" icon={faListOl} active={extended.extended1} onClick={()=> setExtended({extended1: !extended.extended1, extended2:false,  extended3:false,})} >{t(`Level der Eigenständigkeit`)}</Button>
+                        <Button icon={faEnvelopeOpenText} active={extended.extended2} onClick={()=> setExtended({extended2: !extended.extended2, extended1:false,  extended3:false,})}>{t(`Erhaltene Fremdeinschätzung `)}</Button>
+                        <Button icon={faEnvelopeOpenText} active={extended.extended3} onClick={()=> setExtended({ extended3: !extended.extended3,  extended1:false,  extended2:false})}>{t(`Fremdeinschätzung einfordern`)}</Button>
                     </div>
                 <SlideDown className="animated fast">
-                    {list &&
+                    {extended.extended1 &&
                         <div className="text-left bg-white px-2">
                             <List />
                         </div>}
                 </SlideDown>
                 <SlideDown className="animated fast">
-                    { extended.extended1 &&
+                    { extended.extended2 &&
                         <div className='row p-2'>
                             <ExternAssessingn />
                         </div>}
                 </SlideDown>
                 < SlideDown className="animated fast">
-                    {extended.extended2 && <ExternAsk onClick={props.toggleExtended}/>}
+                    {extended.extended3 && <ExternAsk onClick={props.toggleExtended}/>}
                 </SlideDown>
                 </div>
                     <div className="col-sm-12  col-sm-6 col-md-7 col-lg-7 mt-sm-3 pr-5" >
