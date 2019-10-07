0044a1894224fd6aae9dcf1f514957ffd99b0174
toma <mohammed.alhassan-altoaama@charite.de>
2019-10-07 17:11:48 +0200

    Images in PracticalsLegendText machen

--- src/Core/LegendTexts.js
+++ src/Core/LegendTexts.js
@@ -102,11 +102,11 @@ const Legends = {
         Main: {
             title: <Trans>Mein Level</Trans>,
             text: <Trans>
-            <p>Hier siehst du eine Übersicht zu den ärztlichen Kern-Tätigkeiten, die du im Laufe des Studiums trainieren wirst. Nutze diese Funktion, um die Entwicklung deiner praktischen Kompetenz festzuhalten und ggf. nachzusteuern. Für die Einschätzung jeder Tätigkeit gibt es 6 Level, die als Balken dargestellt undauf der rechten Seite in der Legende definiert sind. Das Ziel ist, dass du als Absolvent*in des Medizinstudiums dazu in der Lage bist, die hier aufgeführten Tätigkeiten zu Berufsbeginn eigenständig durchzuführen und nur Wichtiges nachgeprüft wird (Level 5).</p>
+            <p>Hier siehst du eine Übersicht zu den ärztlichen Kern-Tätigkeiten, die du im Laufe des Studiums trainieren wirst. Nutze diese Funktion, um die Entwicklung deiner praktischen Kompetenz festzuhalten und ggf. nachzusteuern. Für die Einschätzung jeder Tätigkeit gibt es 6 Level, die als Balken dargestellt<span className='practicalsImage1'></span>und auf der rechten Seite in der Legende definiert sind. Das Ziel ist, dass du als Absolvent*in des Medizinstudiums dazu in der Lage bist, die hier aufgeführten Tätigkeiten zu Berufsbeginn eigenständig durchzuführen und nur Wichtiges nachgeprüft wird (Level 5).</p>
             <strong>Schätze Dich selber ein</strong>
-            <p>So kannst du im Laufe deines Studiums zum Beispiel nach U- oder UaK-Kursen oder Famulaturen angeben, unter welchem Level du die jeweilige Tätigkeit ausgeführt hast und unter welchem Level du dir die Tätigkeit zutraust</p>
+            <p>So kannst du im Laufe deines Studiums zum Beispiel nach U- oder UaK-Kursen oder Famulaturen angeben, unter welchem Level du die jeweilige Tätigkeit ausgeführt hast <span className='practicalsImage2'></span> und unter welchem Level du dir die Tätigkeit zutraust<span className='practicalsImage3'></span> </p>
             <strong>Bitte um eine Fremdeinschätzung</strong>
-            <p>Es ist zudem möglich, Fremdeinschätzungen von deinen Lehrenden/ Ärzt*innen einzuholen. Hierfür klickst du auf diesen Button  und kannst die E-Mail-Adresse deines Dozierenden eingeben und eine Einladung senden. Unter der E-Mail-Funktion siehst du alle bisherigen Fremdeinschätzungen.</p>
+            <p>Es ist zudem möglich, Fremdeinschätzungen von deinen Lehrenden/ Ärzt*innen einzuholen <span className='practicalsImage4'></span>. Hierfür klickst du auf diesen Button <span className='practicalsImage5'></span>  und kannst die E-Mail-Adresse deines Dozierenden eingeben und eine Einladung senden. Unter der E-Mail-Funktion siehst du alle bisherigen Fremdeinschätzungen.</p>
             </Trans>
         }
     }   
