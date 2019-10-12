f5b0995768fe0481a5ec7fe3a3682d6b7238e727
towma <46755251+towma@users.noreply.github.com>
2019-10-12 14:18:31 +0200

    „Theoretische Fächer“/“Querschnittsfächer“ in Ptm responsive machen

--- src/Exams/Ptm/Subject.js
+++ src/Exams/Ptm/Subject.js
@@ -37,10 +37,10 @@ const Subject = props => (
             <div className="col-7 col-sm-6">
                 <span className="text-primary">#{props.rank}</span><span className="ml-1 font-weight-bold">{props.name}</span>
             </div>
-            <div className="col-5 col-sm-2">
+            <div className="col-5 col-sm-2 p-0">
                 <span className="badge badge-info" style={{fontSize: '.6rem'}}>{props.category}</span>
             </div>
-            <div className="col-10 col-sm-3">
+            <div className="col-10 col-sm-3 mt-1">
                 <SimpleBar value={props.data.richtig} total={props.data.gesamt}>
                     {props.data.richtig} von {props.data.gesamt} richtig
                 </SimpleBar>
