affca83d9147bdfcb7793fa41f08352157a84bca
towma <46755251+towma@users.noreply.github.com>
2019-10-12 14:37:19 +0200

    Das Menü (Bootstrap) in Dashboard klappt sich bei schmalem Fenster automatisch

--- src/Core/navbar.js
+++ src/Core/navbar.js
@@ -14,27 +14,27 @@ export default withTranslation()(makeExtendable(function Navbar({t, ...props}) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white sticky-top">
             <Link className="navbar-brand" to="/">LevelUp</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <FontAwesomeIcon onClick={props.toggleExtended} icon={faBars} />
+            <button className="navbar-toggler" onClick={props.toggleExtended} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <FontAwesomeIcon  icon={faBars} />
             </button>
             <div className={`collapse navbar-collapse ${props.extended && 'show'}`} id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
+                    <li className="nav-item" onClick={props.toggleExtended}>
                         <NavLink className="nav-link" to="/dashboard">{t(`Dashboard`)}<span className="sr-only">(current)</span></NavLink>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item" onClick={props.toggleExtended}>
                         <NavLink className="nav-link" to="/exams">{t(`Prüfungen`)}</NavLink>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item" onClick={props.toggleExtended}>
                         <NavLink className="nav-link" to="/practicals">{t(`Ärztliche Tätigkeiten`)}</NavLink>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item" onClick={props.toggleExtended}>
                         <NavLink className="nav-link" to="/consulting">{t(`Beratung`)}</NavLink>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item" >
                         <a className="nav-link" href='https://msm-tellme.charite.de/lernen'>{t(`TellMe`)}</a>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item" onClick={props.toggleExtended}>
                         <NavLink className="nav-link" to="/willcommen">{t(`1.LogIn`)}</NavLink>
                     </li>
                 </ul>
