04975effc78ba9e7e7cf53a2f2207c3a8c6504ea
Vincent <jan-vincent.wyszynski@charite.de>
2019-10-15 13:31:22 +0200

    clean up DashboardCard

--- src/Dashboard/DashboardCard.js
+++ src/Dashboard/DashboardCard.js
@@ -5,7 +5,7 @@ export default function DashboardCard(props) {
         <div className={`${props.color} card dashboard-card with-border overflow-hidden h-100`}>
           <div className="card-body d-flex p-0">
             <div className={`${props.noPadding || 'p-3'} w-100`}>
-                <h5 className="card-title">{props.title} {props.result && <span className="mr-3 float-right" style={{color: 'red'}}>{props.result}</span>}</h5>
+                {props.title && <h5 className="card-title">{props.title}</h5>}
                 {props.text && <p style={{fontSize: '.8rem'}} className="card-text">{props.text}</p>}
                 {props.children}
             </div>
