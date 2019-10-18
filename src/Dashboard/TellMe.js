8a7dcc2b18fadcbbea54fd2e41b2c03bb0a70b2a
towma <46755251+towma@users.noreply.github.com>
2019-10-18 11:35:10 +0200

    Tellme LegendeText ändern

--- src/Dashboard/TellMe.js
+++ src/Dashboard/TellMe.js
@@ -2,23 +2,26 @@ import React from 'react'
 import DashbordCard from './DashboardCard'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faDumbbell} from '@fortawesome/free-solid-svg-icons'
+import legends from '../Core/LegendTexts'
+import {withTranslation} from 'react-i18next'
 
-const tellme =()=> (
+const Tellme =({t,})=> (
     <div>
         <DashbordCard>
-            <div className='tellMeLogo pb-1 mb-2'></div>
-            <p>Hier kannst du dein aktuelles Wissen anhand von Prüfungsfragen testen und erhältst Rückmeldungen zu deinen Antworten. Pilotphase: TellMe ist gerade erst an den Start gegangen und braucht noch ein wenig Unterstützung. Daher sind aktuell nur die Fragen aus Modul 17 vollständig einsehbar und kommentiert. Wenn du das ändern möchtest, dann mach mit und kommentiere zusammen mit Kommiliton*innen und XX weitere Fragen mit deinen Lösungsvorschlägen.</p>
+            <div className='tellMeLogo pb-1 mb-3'></div>
+            <div>{legends.TellMe.text}</div>
                 <div className='row'> 
-                    <div className='col-12 '>
-                        <span className='font-weight-bold ' style={{borderBottom:'2px solid black'}}>Neues:</span>
-                        <p className='mt-2'>Aktulle ...Version M17 mit kommentierten Fragen. </p>
+                    <div className='col-12'>
+                        <span className='font-weight-bold ' style={{borderBottom:'2px solid black'}}>{t(`Neues`)}:</span>
+                        <p className='mt-2'>Unsere ersten Lerneinheiten für Dich sind fertig: M14 & M17</p>
                     </div>
                         <div className='col-12 col-sm-6 '>
                         <a className='btn btn-secondary form-control mt-2' style={{position:'relative', bottom:'0'}} target="blank" href='https://msm-tellme.charite.de/lernen' >
-                        Trainieren  <span className='ml-2'><FontAwesomeIcon icon={faDumbbell}/></span></a>
+                          {t(`Trainieren`)}<span className='ml-2'><FontAwesomeIcon icon={faDumbbell}/></span>
+                        </a>
                     </div>
                 </div>
         </DashbordCard>
     </div>
 )
-export default tellme
+export default withTranslation()(Tellme)
