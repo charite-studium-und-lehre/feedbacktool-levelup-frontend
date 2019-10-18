523138400965c93e6c5c6ed521a1d4618bbdbbb2
Monika Domanska <monika.domanska@informatik.hu-berlin.de>
2019-10-18 18:10:57 +0200

    small enhencements in EPAs page, and embedded video

--- src/EPAs/Container.js
+++ src/EPAs/Container.js
@@ -1,41 +1,36 @@
 import React from 'react'
 import _ from 'lodash/fp'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import Item from './Item'
 import Tabs from '../Core/Tabs'
 import Legend from '../Charting/Legend'
 import Legends from '../Core/LegendTexts'
 import needsData from '../Core/needsData'
-import Toolbar from './Toolbar'
 import Assessment from './Assessment'
-import { selectors, actions } from './Store'
-const stateToProps = state => ({ filter: selectors.getFilter(state), root: selectors.getItemByLabel(state, 'root') })
+import {selectors, actions} from './Store'
 
-const Title = connect((state, ownProps) => ({ entry: selectors.getItemById(state, ownProps.entryId) }))( props => props.entry.label )
+const stateToProps = state => ({filter: selectors.getFilter(state), root: selectors.getItemByLabel(state, 'root')})
 
-const Container = _.compose([needsData(selectors.loaded, actions.load), connect(stateToProps)])(({ root }) =>
-    <div>
-        <Toolbar/>
+const Title = connect((state, ownProps) => ({entry: selectors.getItemById(state, ownProps.entryId)}))(props => props.entry.label)
+
+const Container = _.compose([needsData(selectors.loaded, actions.load), connect(stateToProps)])(({root}) =>
+    <div className="card mt-2 p-1">
         <Assessment/>
-        <div className="card mt-2 p-1">
-        <div className='row'>
-        </div>
-            <Tabs>
-            { root.entries.map(e =>
-                <div key={e} className="p-2" title={<Title entryId={e} />}>
-                    <Item entryId={e} /> 
+        <Tabs>
+            {root.entries.map(e =>
+                <div key={e} className="p-2" title={
+                    <Title entryId={e}/>
+                }>
+                    <Item entryId={e}/>
                 </div>
             )}
-            </Tabs>
-        </div>
+        </Tabs>
     </div>)
 
-const LegendWrapper = () =>
-    <div style={{ fontSize: '.9rem' }}>
+export default () =>
+    <div style={{fontSize: '.9rem'}}>
         <div className="card p-2">
             <Legend title={Legends.EPAs.Main.title}>{Legends.EPAs.Main.text}</Legend>
         </div>
-        <Container />
+        <Container/>
     </div>
-
-export default LegendWrapper
