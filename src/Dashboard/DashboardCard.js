e24005dff45fa2229055a81b08d755e8dcb910c5
Vincent <jan-vincent.wyszynski@charite.de>
2019-10-18 13:08:01 +0200

    remove trend indicator for ptm dashboard card

--- src/Dashboard/DashboardCard.js
+++ src/Dashboard/DashboardCard.js
@@ -3,11 +3,15 @@ import Legend from '../Charting/Legend'
 
 export default function DashboardCard(props) {
     return (
-        <div className={`${props.color} card dashboard-card with-border overflow-hidden h-100`}>
+        <div className={`${props.color} card dashboard-card with-border overflow-hidden h-100 mt-2`}>
           <div className="card-body d-flex p-0">
-            <div className={`${props.noPadding || 'p-3'} w-100`}>
-                {(props.title || props.text) && <Legend title={props.title}>{props.text}</Legend>}
-                {props.children}
+            <div className={`${props.noPadding || 'p-3'} w-100 d-flex flex-column`}>
+                <div>
+                  {(props.title || props.text) && <Legend title={props.title}>{props.text}</Legend>}
+                </div>
+                <div className="flex-grow-1">
+                  {props.children}
+                </div>
             </div>
           </div>
         </div>
--- src/Dashboard/Ptm.js
+++ src/Dashboard/Ptm.js
@@ -6,7 +6,6 @@ import _ from 'lodash/fp'
 import { scaleLinear } from 'd3-scale'
 import tinycolor from 'tinycolor2'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import needsData from '../Core/needsData'
 import { selectors, actions } from '../Exams/Ptm/Store'
 import DashboardCard from './DashboardCard'
@@ -16,34 +15,26 @@ import AreaGraph from '../Charting/AreaGraph'
 import { color } from '../Exams/Ptm/Ptm'
 import { labels } from '../Exams/Ptm/Results'
 
-const colorScale = scaleLinear([-20,0,20], ['red', tinycolor('yellow').darken(5).toString(), 'green']).clamp(true)
-const rotationScale = scaleLinear([-20,20], [45, -45]).clamp(true)
-
 const stateToProps = state => ({ latest: selectors.getLatest(state), ptms: selectors.getTimeline(state) })
 const Ptm = _.compose(needsData(selectors.loaded, actions.load), connect(stateToProps))(
     ({ latest, ptms }) => {
-    const [ trend, setTrend ] = useState(_.flow(_.map(p => p.result),_.takeRight(2),([a,b]) => b-a)(ptms))
-    return <Link to={`/exams/ptm/${latest.id}`}>
-        <div className="" style={{minHeight: '5rem'}}>
-            <div className="position-absolute" style={{top: 0, bottom: 0, left: 0, right: 0}}>
+    return <div className="h-100" style={{minHeight: '5rem'}}>
+        <Link to={`/exams/ptm/${latest.id}`}>
+            <div className="h-100">
                 <div className="position-absolute pl-2" style={{fontSize: '.75rem', top: '3rem'}}>
-                    <div><span className="font-weight-bold" onClick={e => {e.preventDefault(); setTrend(trend + 1)}}>{labels[0]}: </span>{latest.results[0][0]}</div>
-                    <div><span className="font-weight-bold" onClick={e => {e.preventDefault(); setTrend(trend - 1)}}>{labels[1]}: </span>{latest.results[1][0]}</div>
+                    <div><span className="font-weight-bold">{labels[0]}: </span>{latest.results[0][0]}</div>
+                    <div><span className="font-weight-bold">{labels[1]}: </span>{latest.results[1][0]}</div>
                     <div><span className="font-weight-bold">{labels[2]}: </span>{latest.results[2][0]}</div>
                 </div>
-                <div className="position-absolute pl-2 d-flex align-items-center" style={{right: '2rem', top: '2rem', fontSize: '.9rem', color: colorScale(trend)}}>
-                    <span className="mr-1"><FontAwesomeIcon style={{transform: `rotate(${rotationScale(trend)}deg)`, fontSize: '1.4rem'}} icon={faArrowRight} /></span>
-                    <span className="">{trend >= 0 ? `+${trend}` : trend} %</span>
-                </div>
-                <div className="w-100 h-100 pt-5">
+                <div className="w-100 h-100">
                     <LinearChart xDomain={[0, ptms.length - 1]} yDomain={[0,Math.max(...ptms.map( d => d.result ))]}>
                         <AreaGraph color={tinycolor(color).setAlpha(.08).toString()} data={ptms.map( (d, i) => ({ x: i, y0: 0, y1: d.result }))} />
                         <LineGraph color={tinycolor(color).setAlpha(.23).toString()} data={ptms.map( (d, i) => ({ x: i, y: d.result }))} />
                     </LinearChart>
                 </div>
             </div>
-        </div>
-    </Link>
+        </Link>
+    </div>
 })
 
 const Wrapper = withTranslation()(({ t }) => <DashboardCard 
