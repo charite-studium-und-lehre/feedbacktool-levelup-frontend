4b8dece7f8755c911b104469a4c23b85ddcc6291
Vincent <jan-vincent.wyszynski@charite.de>
2019-10-24 16:32:52 +0200

    fix minor bugs

--- src/Dashboard/Newsfeed/NewsfeedMobile.js
+++ src/Dashboard/Newsfeed/NewsfeedMobile.js
@@ -4,21 +4,13 @@ import { connect } from 'react-redux'
 import needsData from '../../Core/needsData'
 import { selectors, actions } from './Store'
 import Item from './Item'
-import { Separator } from './Newsfeed'
 import { withTranslation } from 'react-i18next'
 
 const stateToProps = state => ({ data: selectors.getData(state) })
 const Newsfeed = _.compose([needsData(selectors.loaded, actions.load), connect(stateToProps), withTranslation()])(({ t, data }) => 
     <div className="pb-3">
         <div className="text-center">
-        {_.size(data) ? 
-            <div className="d-flex">
-                <Separator />
-                <div style={{color: 'grey'}} className="font-italic">{data[0].zeitsemester}</div>
-                <Separator />
-            </div> :
-            <div className="p-3">{t('Hier werden zukünftig deine Prüfungen angezeigt.')}</div>
-        }
+        { !_.size(data) && <div className="p-3">{t('Hier werden zukünftig deine Prüfungen angezeigt.')}</div> }
         </div>
         { !!_.size(data) && data[0].map( d => 
             <Item key={d.link} icon={d.icon} color={d.color} link={d.link}>
