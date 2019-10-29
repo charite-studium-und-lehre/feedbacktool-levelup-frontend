5e581b8d11e1dbab9b605ebfe99371deb12ff89d
Vincent <jan-vincent.wyszynski@charite.de>
2019-10-29 17:29:23 +0100

    #2354 change ptm dashboard dard numbers

--- src/Dashboard/Ptm.js
+++ src/Dashboard/Ptm.js
@@ -17,23 +17,23 @@ const stateToProps = state => ({ latest: selectors.getLatest(state), ptms: selec
 const Ptm = _.compose(needsData(selectors.loaded, actions.load), connect(stateToProps), withTranslation())(
     ({ t, latest, ptms }) => {
     return latest ? <div className="h-100" style={{minHeight: '5rem'}}>
-        <Link to={`/exams/ptm/${latest.id}`}>
+        <Link to={`/exams/ptms/${latest.id}`}>
             <div className="h-100 position-relative">
-                <div className="position-absolute pl-3" style={{fontSize: '.75rem', top: '3rem'}}>
+                <div className="position-absolute pl-3" style={{fontSize: '.75rem', top: '.7rem'}}>
                     {labels.map( (l,i) => 
-                        <div key={i}>
-                            <span style={{fontSize: '1.1rem', fontWeight: 500}} className="mr-1">{latest.results[i]}</span>
+                        <div className="d-inline-block mr-4" key={i} style={{lineHeight: '1.9rem'}}>
+                            <span style={{fontSize: `${(2-i)*.5+1.4}rem`, fontWeight: 500}} className="mr-1">{latest.results[i]}</span>
                             {l}
                         </div>
                     )}
                 </div>
                 <div className="w-100 h-100">
                     <LinearChart xDomain={[ptms.length === 1 ? -1 : 0, ptms.length - 1]} yDomain={[0,Math.max(...ptms.map( d => d.results[0] ))]}>
-                        <AreaGraph color={tinycolor(color).setAlpha(.08).toString()} data={
+                        <AreaGraph color={tinycolor(color).setAlpha(.12).toString()} data={
                             ptms.map( (d, i) => ({ x: i, y0: 0, y1: d.results[0] }))
                                 .concat(ptms.length === 1 ? [{ x: -1, y0: 0, y1: 0 }] : [])
                         } />
-                        <LineGraph color={tinycolor(color).setAlpha(.23).toString()} data={
+                        <LineGraph color={tinycolor(color).setAlpha(.27).toString()} data={
                             ptms.map( (d, i) => ({ x: i, y: d.results[0], selected: d.id === latest.id }))
                                 .concat(ptms.length === 1 ? [{ x: -1, y: 0, size: .01 }] : [])
                         } />
