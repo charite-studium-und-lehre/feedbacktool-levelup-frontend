a059f0104baafbc7b9401cd13af78ba316b39b10
Vincent <jan-vincent.wyszynski@charite.de>
2019-10-31 10:30:41 +0100

    change feedback welcome message

--- src/Core/Feedback.js
+++ src/Core/Feedback.js
@@ -11,7 +11,7 @@ const url = 'https://lu-feedback.herokuapp.com/'
 const socket = socketio(url)
 
 const style = {
-    position: 'absolute',
+    position: 'fixed',
     bottom: 0,
     right: 0,
     fontSize: '.8rem',
@@ -29,7 +29,7 @@ const Feedback = _.compose([connect(stateToProps), waitForLogin])(({ user }) => 
     const [ ts, setTs ] = useState(window.localStorage.getItem('ts'))
     const [ show, setShow ] = useState(false)
     const [ messages, setMessages ] = useState([{
-        text: 'Das hier ist LevelUp! Wir wollen dir auf dieser Plattform in Zukunft Rückmeldung zu deinem Fortschritt im Studium geben. Daher würden wir gerne deine Meinung dazu hören, was wir noch verbessern können. Schreib uns hier im Textfeld einfach alles, was dir einfällt. Bitte beachte, dass LevelUp noch nicht fertig ist und es noch ein paar Fehler geben kann. Außerdem werden hier nur Beispieldaten angezeigt, die nichts mit deinen echten Ergebnissen zu tun haben.',
+        text: 'Willkommen auf LevelUp! Hier hast du die Möglichkeit uns Feedback zu senden. Schreib uns einfach alles, was dir einfällt.',
         sender: 'server'
     }])
 
@@ -70,25 +70,27 @@ const Feedback = _.compose([connect(stateToProps), waitForLogin])(({ user }) => 
     }
 
     return <div style={style} className="p-4">
-        { show && <div className="with-shadow" style={{width: '18rem', backgroundColor: 'white', border: 'none'}}>
+        { show ? <div className="with-shadow" style={{width: '18rem', backgroundColor: 'white', border: 'none'}}>
             <div className="text-right px-2 color-bg-navigation" style={{height: '2rem', lineHeight: '2.1rem'}} onClick={() => setShow(false)} >
                 <FontAwesomeIcon style={{fontSize: '1rem'}} className="text-white" icon={faTimesCircle} />
             </div>
             <div ref={messageDiv} className="p-2 overflow-auto" style={{maxHeight: '17rem'}}>
                 {messages.map((msg, i) => <div className={`mt-1 text-white ${msg.sender === 'server' && 'text-right'}`} key={i}>
-                    <span style={{borderRadius: '.3rem'}} className={`text-left d-inline-block p-2 ${msg.sender === 'server' ? 'bg-info ml-3' : 'bg-secondary mr-3'}`}>{msg.text}</span>
+                    <span style={{borderRadius: '.3rem'}} className={`text-left d-inline-block p-2 ${msg.sender === 'server' ? 'bg-info ml-3' : 'bg-secondary mr-3'}`}>
+                        {msg.text}
+                    </span>
                 </div>)}
             </div>
             <div className="text-right p-2 pb-0 d-flex">
-                <input style={{border: 'none', borderBottom: '1px solid lightgrey'}} className="flex-grow-1 no-outline" placeholder="Dein Feedback..." ref={input} onKeyUp={e => handleKeyPress(e)}></input>
+                <input style={{border: 'none', borderBottom: '1px solid lightgrey'}} className="flex-grow-1 no-outline" placeholder="Dein Feedback..." ref={input} onKeyUp={e => handleKeyPress(e)} />
                 <FontAwesomeIcon className="text-primary m-2" style={{fontSize: '1rem'}} icon={faPaperPlane} onClick={ () => postFeedback() } />
             </div>
             <div className="text-right p-2 pb-3 d-flex" style={{cursor: 'pointer', lineHeight: '1.2em', color: 'grey', fontSize: '.7rem'}} onClick={ () => setSendId(!sendId) }>
                 <FontAwesomeIcon className="mr-1" style={{fontSize: '1.2em'}} icon={sendId ? faCheckSquare : faSquare} />
-                meine Email-Adresse mitsenden
+                meine Mailadresse mitsenden
             </div>
-        </div>}
-        {!show && <div className="w-100 text-right with-shadow">
+        </div> :
+        <div className="w-100 text-right with-shadow">
             <button className="btn color-button-color" onClick={ () => setShow(true) }>
                 <FontAwesomeIcon style={{fontSize: '1rem'}} icon={faEnvelope} />
             </button>
