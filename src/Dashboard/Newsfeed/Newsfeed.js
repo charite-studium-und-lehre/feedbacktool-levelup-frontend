be9a4a80a66154732ccbdf6bf583ad05972784e5
Vincent <jan-vincent.wyszynski@charite.de>
2019-10-31 15:24:24 +0100

    fix newsfeed x scrollbar

--- src/Dashboard/Newsfeed/Newsfeed.js
+++ src/Dashboard/Newsfeed/Newsfeed.js
@@ -13,8 +13,8 @@ export const Separator = () => <div className="px-2 flex-grow-1 d-flex align-ite
 
 const stateToProps = state => ({ data: selectors.getData(state) })
 const Newsfeed = _.compose([needsData(selectors.loaded, actions.load), connect(stateToProps), withTranslation()])(({ t, data }) => 
-    <div className="position-relative h-100" style={{minHeight: '10rem'}}>
-        <div className="position-absolute overflow-auto p-1" style={{top: 0, bottom: 0, left: 0, right: 0}}>
+    <div className="position-relative h-100" style={{minHeight: '10rem', overflowX: 'hidden', overflowY: 'auto'}}>
+        <div className="position-absolute" style={{top: 0, bottom: 0, left: 0, right: 0}}>
             { _.size(data) ? _.map( g => 
                 <div key={g[0].zeitsemester}>
                     <div className="text-center font-italic mt-3 d-flex" style={{ fontSize: '.8rem' }}>
