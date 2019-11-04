107727eaa7956f2506e8224c601deb6bb93d13f8
toma <mohammed.alhassan-altoaama@charite.de>
2019-11-04 16:50:02 +0100

    Epas LegendeText responsive machen bzw. den code umschreiben

--- src/EPAs/static/CheatSheetCard.js
+++ src/EPAs/static/CheatSheetCard.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { withTranslation } from 'react-i18next'
 import makeExtendable from '../../Core/makeExtendable'
 import Legend from "../../Charting/Legend"
+import Level from '../assessmentViewComponents/Level'
+import COLORS from '../../colors';
 
 import Legends from "../../Core/LegendTexts";
 
@@ -9,7 +11,12 @@ const CheatSheetCard = () => {
     return (
         <div className="card p-3 mt-2">
             <Legend title={Legends.EPAs.CheatSheetCard.levels.title}>
-                {Legends.EPAs.CheatSheetCard.levels.text}
+                {Legends.EPAs.CheatSheetCard.levels.text.map((e, d) =>
+                    <div key={d}>
+                        <div>{`${d} - ${e}`}</div>
+                        <Level color={COLORS.background.grey7} colorBackground={COLORS.background.grey1} value={d}/>
+                    </div>)
+            }
             </Legend>
             <p style={{color: "white", lineHeight:"1rem"}}> </p>
             <Legend title={Legends.EPAs.CheatSheetCard.video.title}>
