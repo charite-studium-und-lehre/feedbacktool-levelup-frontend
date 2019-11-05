e6bda7d0077001ffc53ce5c5fdf79a5fdec915ba
Vincent <jan-vincent.wyszynski@charite.de>
2019-11-05 18:40:03 +0100

    fix warning about Tabs Component using componentWillUpdate

--- src/Utils/Tabs.js
+++ src/Utils/Tabs.js
@@ -1,49 +1,25 @@
-import React, { Component } from 'react'
-import COLORS from "../colors";
+import React, { useState } from 'react'
+import COLORS from "../colors"
 
-const asTabs = WrappedComponent => {
-    return class Tabs extends Component {
-        constructor(props) {
-            super(props)
-            this.state = { tab: Math.max(...React.Children.map(props.children, (child, i) => props.active && child.props.title === props.active ? i : 0)) }
-            this.activeTab = React.createRef()
-        }
+const Tab = ({ content, selected }) => 
+    <div className="align-top w-100 d-inline-block" style={{whiteSpace: 'normal', height: selected ? 'auto' : 0}} >
+        {content}
+    </div>
 
-        componentDidMount() {
-            this.updateContainerHeight()
-        }
+const asTabs = HeaderComponent => 
+    ({ children, active, ...otherProps}) => {
+        const [tab,setTab] = useState(Math.max(...React.Children.map(children, (child, i) => active && child.props.title === active ? i : 0)))
 
-        componentDidUpdate() {
-            this.updateContainerHeight()
-        }
-        
-        componentWillUpdate() {
-            if(this.activeTab.current) this.activeTab.current.style.height = 0
-        }
-
-        updateContainerHeight() {
-            if(this.activeTab.current) this.activeTab.current.style.height = 'auto'
-        }
-
-        selectTab( tab ) {
-            this.setState({ tab })
-        }
-
-        render() {
-            return (
-                <div className="py-2" style={{backgroundColor: COLORS.background.grey0}}>
-                    <WrappedComponent selectTab={tab => this.selectTab(tab)} selectedTab={this.state.tab} {...this.props} />
-                    <div className="text-nowrap animated pt-4" style={{overflow: `hidden`, backgroundColor: "white"}} >
-                        <div className="animated position-relative" style={{left: `-${this.state.tab * 100}%`}}>
-                        {React.Children.map(this.props.children, (tc, i) =>
-                            <div className="align-top w-100 d-inline-block" key={i} style={{whiteSpace: 'normal', height: 0}} ref={i === this.state.tab ? this.activeTab : null}>{tc}</div>
-                        )}
-                        </div>
+        return (
+            <div className="py-2" style={{backgroundColor: COLORS.background.grey0}}>
+                <HeaderComponent selectTab={tab => setTab(tab)} selectedTab={tab} {...otherProps}>{children}</HeaderComponent>
+                <div className="text-nowrap animated pt-4" style={{overflow: `hidden`, backgroundColor: "white"}} >
+                    <div className="animated position-relative" style={{left: `-${tab * 100}%`}}>
+                    {React.Children.map(children, (tc, i) => <Tab key={i} selected={i === tab} content={tc} /> )}
                     </div>
-                </div>)
-        }
+                </div>
+            </div>)
     }
-}
 
 export default asTabs(props =>
     <ul className="nav nav-pills" role="tablist">
