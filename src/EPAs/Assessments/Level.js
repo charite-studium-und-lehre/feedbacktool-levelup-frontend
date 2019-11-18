f9e7b5dd3cdeb002882e0b4564f7366a7ae1c528
Toma <m.towma@hotmail.com>
2019-11-18 17:32:44 +0100

    increment und decrement icons in Epas bearbeiten .

--- src/EPAs/Assessments/Level.js
+++ src/EPAs/Assessments/Level.js
@@ -2,6 +2,7 @@ import React from 'react'
 import _ from 'lodash'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
+import { tupleTypeAnnotation } from '@babel/types'
 
 const Square = props=> {
     const defaultStyle =  {
@@ -26,17 +27,20 @@ const Level = ({ maxValue = 5, value = 0, color, colorBackground, ...props }) =>
     </div>
 }
 
-export const LevelWithEdit = ({ increment, decrement, ...otherProps}) => 
-    <div>
-        <span style={{ cursor: 'pointer' }} className={(!decrement || otherProps.value < 1) ? 'invisible' : 'visible'}>
-            <FontAwesomeIcon icon={faMinusCircle} className="text-muted" onClick={decrement} />
+export const LevelWithEdit = ({ increment, decrement, ...otherProps}) => {
+    const test = ()=> alert('ff')
+    const condition1 = otherProps.value < 1
+    const condition2 = otherProps.value >= otherProps.maxValue
+     return <div>
+        <span style={{ cursor: condition1 ? 'auto': 'pointer' , color: condition1 ? 'rgb(108, 117, 125,.2)':'rgb(108, 117, 125)' }}>
+            <FontAwesomeIcon icon={faMinusCircle} onClick={!condition1 ? decrement :''} />
         </span>
         <span className="mx-1">
             <Level {...otherProps} />
         </span>
-        <span style={{ cursor: 'pointer' }} className={(!increment || otherProps.value >= otherProps.maxValue) ? 'invisible' : 'visible'}>
-            <FontAwesomeIcon icon={faPlusCircle} className="text-muted" onClick={increment} />
+        <span style={{cursor: condition2 ? 'auto': 'pointer', color: condition2 ? 'rgb(108, 117, 125,.2)':'rgb(108, 117, 125)' }}>
+            <FontAwesomeIcon icon={faPlusCircle}  onClick={!condition2 ? increment :'' } />
         </span>
     </div>
-
+}
 export default Level
