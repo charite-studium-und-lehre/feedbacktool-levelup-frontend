e72e0026c211375d4c7ab95114918077ef328240
toma <mohammed.alhassan-altoaama@charite.de>
2019-11-19 17:37:07 +0100

    AllTools neuer Emtwurf

--- src/alltools/AllTools.js
+++ src/alltools/AllTools.js
@@ -1,75 +1,96 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import {withTranslation} from 'react-i18next'
-
-import {StudyToolsData} from './StudyToolsData'
-import {OtherToolsData} from './OtherToolsData'
-
+import {StudyToolsData, OtherToolsData } from './AllToolsData'
 import stift from './stift.svg'
 import spritze from './spritze.svg'
 
-function AllTools({t}) {
+const  MakeStairsList = props => (
+    <div className='col-12 col-lg-6'>
+          <ul className="list-group list-group-flush">
+            {props.data.map((d, i) => <div>
+             <h4 className="font-weight-bold" style={{paddingLeft: `${i}rem`}}>{d.title}</h4> 
+            <a className="font-weight-bold list-group-item " style={{color:'rgb(34, 71, 104)',paddingLeft: `${i}rem`}} key={i} href={d.href}>{d.href}</a>
+            </div>)}
+         </ul>
+    </div>
+)
 
-    let makeStairsList = dataArray => dataArray.map((data, i, array) =>
-        <div key={i} style={{marginBottom: "1em"}}>
-            <div style={{marginLeft: (array.length-i)+"em"}} >
-                <h4 style={{marginBottom: "0px"}}>{data.title}</h4>
-                <a href={data.href} className="color-navigation">{data.href}</a>
-            </div>
-        </div>
-    );
-
-    const studyCards = makeStairsList(StudyToolsData);
-    const othersCards = OtherToolsData.map((data, i) =>
-        <div key={i}>
-                <h4 style={{marginBottom: "0px"}}>{data.title}</h4>
-                {data.intern ? 
-                    <Link to={data.href} className="color-navigation">{data.hrefLabel || data.href}</Link> : 
-                    <a href={data.href} className="color-navigation">{data.hrefLabel || data.href}</a>
-                }
-        </div>
-    );
-
-    return (
-        <div>
-            <h1>{t('Lern-Tools')}</h1>
-
-            <div style={{position: "relative"}}>
-                <img src={stift} alt='' style={{
-                    position: "absolute",
-                    minWidth: "10%",
-                    maxWidth: "200px",
-                    height: "80%",
-                    transform: "rotate(17deg) translate(50px)",
-                    zIndex: -100
-                }}/>
-                <div style={{
-                    width: "100%",
-                    height: "100%",
-                    position: "absolute",
-                    zIndex: -100
-                }}>
-                    <img src={spritze} alt='' style={{
-                        minWidth: "10%",
-                        maxWidth: "200px",
-                        height: "80%",
-                        right: "1%",
-                        bottom: 0,
-                        position: "absolute"
-                    }}/>
-                </div>
-                <div style={{margin: "auto", maxWidth: "80%", zIndex: 100}}>
-                    {studyCards}
-                </div>
-            </div>
-
-            <h1>{t('Weiteres')}</h1>
-
-            <div style={{paddingLeft: "2em"}}>
-                {othersCards}
-            </div>
-        </div>
-    )
-}
+const  AllTools = props => (
+    <div className='row'>
+         <MakeStairsList 
+         data={StudyToolsData}/>
+         <MakeStairsList 
+         data={OtherToolsData}/>
+    </div>
+)
 
 export default withTranslation()(AllTools)
+
+
+
+// function AllTools({t}) {
+
+//     let makeStairsList = dataArray => dataArray.map((data, i, array) =>
+//         <div key={i} style={{marginBottom: "1em"}}>
+//             <div style={{marginLeft: (array.length-i)+"em"}} >
+//                 <h4 style={{marginBottom: "0px"}}>{data.title}</h4>
+//                 <a href={data.href} className="color-navigation">{data.href}</a>
+//             </div>
+//         </div>
+//     );
+
+//     const studyCards = makeStairsList(StudyToolsData);
+//     const othersCards = OtherToolsData.map((data, i) =>
+//         <div key={i}>
+//                 <h4 style={{marginBottom: "0px"}}>{data.title}</h4>
+//                 {data.intern ? 
+//                     <Link to={data.href} className="color-navigation">{data.hrefLabel || data.href}</Link> : 
+//                     <a href={data.href} className="color-navigation">{data.hrefLabel || data.href}</a>
+//                 }
+//         </div>
+//     );
+
+//     return (
+//         <div>
+//             <h1>{t('Lern-Tools')}</h1>
+
+//             <div style={{position: "relative"}}>
+//                 <img src={stift} alt='' style={{
+//                     position: "absolute",
+//                     minWidth: "10%",
+//                     maxWidth: "200px",
+//                     height: "80%",
+//                     transform: "rotate(17deg) translate(50px)",
+//                     zIndex: -100
+//                 }}/>
+//                 <div style={{
+//                     width: "100%",
+//                     height: "100%",
+//                     position: "absolute",
+//                     zIndex: -100
+//                 }}>
+//                     <img src={spritze} alt='' style={{
+//                         minWidth: "10%",
+//                         maxWidth: "200px",
+//                         height: "80%",
+//                         right: "1%",
+//                         bottom: 0,
+//                         position: "absolute"
+//                     }}/>
+//                 </div>
+//                 <div style={{margin: "auto", maxWidth: "80%", zIndex: 100}}>
+//                     {studyCards}
+//                 </div>
+//             </div>
+
+//             <h1>{t('Weiteres')}</h1>
+
+//             <div style={{paddingLeft: "2em"}}>
+//                 {othersCards}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default withTranslation()(AllTools)
---
+++ src/alltools/AllToolsData.js
@@ -0,0 +1,53 @@
+export const OtherToolsData = [
+        {
+            title: 'Beratungsangebote',
+            href: '/consulting',
+        },
+        {
+            title: 'Fachschaft Medizin',
+            href: 'http://fsi-charite.de/'},
+        {
+            title: 'Progress Test Medizin (PTM)',
+            href: 'https://progress-test-medizin.charite.de/'},
+        {
+            title: 'Webseite Charité Lehre',
+            href: 'https://www.charite.de/studium_lehre/struktur_einrichtungen/'},
+        {
+            title: 'Evaluation',
+            href: 'https://intranet.charite.de/evaluation/evaluationsergebnisse/',
+        },
+        {
+            title: 'Ordnungen',
+            href: 'https://www.charite.de/studium_lehre/studiengaenge/modellstudiengang_humanmedizin/'}
+    ];
+
+    export const StudyToolsData = [
+        {
+            title: 'LLP',
+            href: 'https://lernziele.charite.de'},
+        {
+            title: 'HIS',
+            href: 'https://his.charite.de'},
+        {
+            title: 'Campusnet',
+            href: 'https://campusnet.charite.de'},
+        {
+            title: 'Blackboard',
+            href: 'https://lms.charite.de'},
+        {
+            title: 'TellMe',
+            href: 'https://msm-tellme.charite.de/lernen/'},
+        {
+            title: 'Minerva (Hausarbeit M23)',
+            href: 'https://minerva.charite.de/'},
+        {
+            title: 'Lernzentrum (Raumbuchung)',
+            href: 'https://lernzentrum.charite.de/leistungen/raumbuchung/'},
+        {
+            title: 'Wikiblog',
+            href: 'https://wikiblog.charite.de'},
+        {
+            title: 'Medizinische Bibliothek',
+            href: 'https://bibliothek.charite.de/'}
+
+    ];
