23df69f420d476a88320561f7daa8a48758fdf21
toma <mohammed.alhassan-altoaama@charite.de>
2020-01-09 12:00:46 +0100

    lightgrey Color hinzufügen

--- src/colors.js
+++ src/colors.js
@@ -63,7 +63,8 @@ const colors = {
         grey2: 'var(--color-background-grey2)',
         grey1: '#CFD8E4',
         grey0: '#e4e9f0',
-        buttonGrey: '#6c757d'
+        buttonGrey: '#6c757d',
+        lightgrey : 'lightgrey'
     },
 
     textBlack: '#101010',
