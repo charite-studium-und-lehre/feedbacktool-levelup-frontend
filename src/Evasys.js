176cc6a9b4f0732528ca7bbb350f70f6362a1f0c
Toma <m.towma@hotmail.com>
2020-01-12 22:37:04 +0100

    UmfrageCard 1. Entwurf

---
+++ src/Evasys.js
@@ -0,0 +1,11 @@
+import React from 'rect'
+
+const Evasy = ()=> (
+<div>
+    <h2>Nutzerumfrage LeverUp</h2>
+    <p>" gib uns Deine Feedbak "</p>
+    <button className='btn'>Mitmachen<span></span></button>
+    
+</div>
+)
+export default Evasy 
