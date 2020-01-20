2dfb4d6b5d343088cd31ee3fac7d1f939a493c29
toma <mohammed.alhassan-altoaama@charite.de>
2020-01-20 15:50:12 +0100

    EvasyText korriegen

--- src/Evasys.js
+++ src/Evasys.js
@@ -10,7 +10,7 @@ const Evasy = () => (
     <div className='card with-border mt-2'>
         <div className='card-body text-center p-0'>
             <h3 className='mt-3'>Nutzerumfrage LevelUp</h3>
-            <h5 className='mt-3'>"gib uns Deine Feedback"</h5>
+            <h5 className='mt-3'>"Gib uns dein Feedback"</h5>
             <div className='mx-auto my-3' style={{ width: '16rem' }}>
                 <div className='mt-4'>
                     <Icon icon={faFrown} />
