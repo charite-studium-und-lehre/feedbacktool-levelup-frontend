8cb3df61eb3c4af0916df7acd2575b301703b532
nika <monika.domanska@charite.de>
2020-02-07 12:07:06 +0100

    layout

--- src/User/Login.js
+++ src/User/Login.js
@@ -27,33 +27,32 @@ const Login = _.compose([
     <div className='container-fluid h-100'>
         <div className='row h-100' style={background}>
             <div className='col-md-5 offset-md-7 bg-white px-3'>
-                <div className=''>
-                    <h3 className='my-4 text-center'>{t(`Willkommen bei LevelUp`)}</h3>
-                    <div className='mt-5'>
-                        <p>{t(`Herzlich Willkommen bei LevelUp – deinem Online Feedback Tool zur integrierten Darstellung deines Studienfortschritts im Modellstudiengang 2.0.`)}</p>
-                    </div>
-                    <Link to='/user/dataProtection'>
-                        <div className='color-navigation'>
-                            <span className='font-weight-bold' style={{ cursor: 'pointer', fontSize: '1rem' }}>{t(`Datenschutzhinweise`)}</span>
-                        </div>
-                    </Link>
+                <h3 className='my-4 text-center'>{t(`Willkommen bei LevelUp`)}</h3>
+                <div className='mb-4'>
+                    <p>{t(`Herzlich Willkommen bei LevelUp – deinem Online Feedback Tool zur integrierten Darstellung deines Studienfortschritts im Modellstudiengang 2.0.`)}</p>
                 </div>
-                <div className="container-form mt-3 px-4">
+                <Link to='/user/dataProtection'>
+                    <div className='color-navigation'>
+                        <span className='font-weight-bold'
+                              style={{cursor: 'pointer', fontSize: '1rem'}}>{t(`Datenschutzhinweise`)}</span>
+                    </div>
+                </Link>
+                <div className="container-form my-3 px-4">
                     <a href="https://levelup.charite.de/backend/login">
                         <button type="button" className="btn btn-secondary w-100 font-weight-bold">{t(`Login`)}</button>
                     </a>
                 </div>
-                <div className="mt-4" style={{fontSize: '.9rem'}}>
+                <div className='mt-b'>
                     <div>{t('Bei Problemen mit dem Login melde dich bitte bei levelup@charite.de.')}</div>
-                    <Link to='/app-demo'>
-                        <div className='color-navigation'>
+                </div>
+                <Link to='/app-demo'>
+                    <div className='color-navigation'>
                             <span className='font-weight-bold' style={{
                                 cursor: 'pointer',
                                 fontSize: '1rem'
                             }}>{t(`Demo-Version von LevelUp ansehen`)}</span>
-                        </div>
-                    </Link>
-                </div>
+                    </div>
+                </Link>
             </div>
         </div>
     </div>
