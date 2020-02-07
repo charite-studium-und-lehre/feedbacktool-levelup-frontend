c76bce50a9a616bd3e9314dbe4dfb053848f8613
nika <monika.domanska@charite.de>
2020-02-07 12:11:29 +0100

    layout

--- src/User/Login.js
+++ src/User/Login.js
@@ -28,7 +28,7 @@ const Login = _.compose([
         <div className='row h-100' style={background}>
             <div className='col-md-5 offset-md-7 bg-white px-3'>
                 <h3 className='my-4 text-center'>{t(`Willkommen bei LevelUp`)}</h3>
-                <div className='mb-4'>
+                <div className='mb-3'>
                     <p>{t(`Herzlich Willkommen bei LevelUp – deinem Online Feedback Tool zur integrierten Darstellung deines Studienfortschritts im Modellstudiengang 2.0.`)}</p>
                 </div>
                 <Link to='/user/dataProtection'>
@@ -42,7 +42,7 @@ const Login = _.compose([
                         <button type="button" className="btn btn-secondary w-100 font-weight-bold">{t(`Login`)}</button>
                     </a>
                 </div>
-                <div className='mt-b'>
+                <div className='mb-3'>
                     <div>{t('Bei Problemen mit dem Login melde dich bitte bei levelup@charite.de.')}</div>
                 </div>
                 <Link to='/app-demo'>
