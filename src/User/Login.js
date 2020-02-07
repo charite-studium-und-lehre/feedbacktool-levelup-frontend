9a79041522260d59a0252e81aa230021978d5b19
nika <monika.domanska@charite.de>
2020-02-07 12:17:34 +0100

    2449 link zur demo auf der login seite hinzugefügt

--- src/User/Login.js
+++ src/User/Login.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import _ from 'lodash/fp'
 import image from './login.jpg'
-import { Link, Redirect } from 'react-router-dom'
-import { withTranslation } from 'react-i18next'
+import {Link, Redirect} from 'react-router-dom'
+import {withTranslation} from 'react-i18next'
 import makeExtendable from '../Core/makeExtendable'
-import { selectors as user } from './Store'
+import {selectors as user} from './Store'
 
 const background = {
     background: `url(${image}) no-repeat center`,
     backgroundSize: 'cover'
 }
 
-const stateToProps = state => ({ 
+const stateToProps = state => ({
     loggedIn: user.isLoggedIn(state),
     hasStammdata: user.getData(state).stammdatenVorhanden,
 })
@@ -21,31 +21,38 @@ const Login = _.compose([
     connect(stateToProps),
     withTranslation(),
     makeExtendable(),
-    C => props => props.loggedIn && !props.hasStammdata ? <Redirect to='/user/registration' /> : <C {...props} />,
-    C => props => props.loggedIn ? <Redirect to='/dashboard' /> : <C {...props} />
-])(({ t }) =>
-    <div className='container-fluid h-100' >
+    C => props => props.loggedIn && !props.hasStammdata ? <Redirect to='/user/registration'/> : <C {...props} />,
+    C => props => props.loggedIn ? <Redirect to='/dashboard'/> : <C {...props} />
+])(({t}) =>
+    <div className='container-fluid h-100'>
         <div className='row h-100' style={background}>
             <div className='col-md-5 offset-md-7 bg-white px-3'>
-                <div className=''>
-                    <h3 className='my-4 text-center'>{t(`Willkommen bei LevelUp`)}</h3>
-                    <div className='mt-5'>
-                        <p >{t(`Herzlich Willkommen bei LevelUp – deinem Online Feedback Tool zur integrierten Darstellung deines Studienfortschritts im Modellstudiengang 2.0.`)}</p>
-                    </div>
-                    <Link to='/user/dataProtection'>
-                        <div className='color-navigation'>
-                            <span className='font-weight-bold' style={{ cursor: 'pointer', fontSize: '1rem' }}>{t(`Datenschutzhinweise`)}</span>
-                        </div>
-                    </Link>
+                <h3 className='my-4 text-center'>{t(`Willkommen bei LevelUp`)}</h3>
+                <div className='mb-3'>
+                    <p>{t(`Herzlich Willkommen bei LevelUp – deinem Online Feedback Tool zur integrierten Darstellung deines Studienfortschritts im Modellstudiengang 2.0.`)}</p>
                 </div>
-                <div className="container-form mt-3 px-4">
+                <Link to='/user/dataProtection'>
+                    <div className='color-navigation'>
+                        <span className='font-weight-bold'
+                              style={{cursor: 'pointer', fontSize: '1rem'}}>{t(`Datenschutzhinweise`)}</span>
+                    </div>
+                </Link>
+                <div className="container-form my-3 px-4">
                     <a href="https://levelup.charite.de/backend/login">
-                        <button type="button" className="btn btn-secondary w-100 font-weight-bold" >{t(`Login`)}</button>
+                        <button type="button" className="btn btn-secondary w-100 font-weight-bold">{t(`Login`)}</button>
                     </a>
                 </div>
-                <div className="mt-4 p-2" style={{fontSize: '.9rem'}}>
-                    {t('Bei Problemen mit dem Login melde dich bitte bei levelup@charite.de.')}
+                <div className='mb-3'>
+                    <div>{t('Bei Problemen mit dem Login melde dich bitte bei levelup@charite.de.')}</div>
                 </div>
+                <Link to='/app-demo'>
+                    <div className='color-navigation'>
+                            <span className='font-weight-bold' style={{
+                                cursor: 'pointer',
+                                fontSize: '1rem'
+                            }}>{t(`Demo-Version von LevelUp ansehen`)}</span>
+                    </div>
+                </Link>
             </div>
         </div>
     </div>
