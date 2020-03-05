0e51db660d140a5f8eb746ebdb59c016dcc09831
toma <mohammed.alhassan-altoaama@charite.de>
2020-03-05 11:27:54 +0100

    LoginImage Fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 import React from 'react'
-import {BrowserRouter, NavLink} from 'react-router-dom'
-import {Route, Redirect} from 'react-router'
-import {Provider} from 'react-redux'
-import {createStore, compose, applyMiddleware} from 'redux'
+import { BrowserRouter, NavLink } from 'react-router-dom'
+import { Route, Redirect } from 'react-router'
+import { Provider } from 'react-redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducer'
 import 'd3-transition'
@@ -16,7 +16,7 @@ import Navbar from './Core/routing/navbar'
 import Breadcrumbs from './Core/routing/Breadcrumbs'
 import PrivateRoute from './Core/routing/PrivateRoute'
 import Routes from './Core/routing/Routes'
-import {withTranslation} from 'react-i18next'
+import { withTranslation } from 'react-i18next'
 import ScrollToTop from './Core/ScrollToTop'
 import COLORS from "./colors"
 
@@ -29,26 +29,24 @@ const App = withTranslation()(() =>
         <BrowserRouter basename={basename}>
             <div className="App p-0 d-flex flex-column">
                 <Navbar />
-                <Breadcrumbs/>
-                <ScrollToTop/>
+                <Breadcrumbs />
+                <ScrollToTop />
                 <div className="flex-fill">
-                    <div>
                     {Routes.map(route => (route.private ?
-                            <PrivateRoute key={route.path} path={route.path} component={route.component}
-                                            exact={route.exact} /> :
-                            <Route key={route.path} path={route.path} component={route.component}
-                                    exact={route.exact}/>
+                        <PrivateRoute key={route.path} path={route.path} component={route.component}
+                            exact={route.exact} /> :
+                        <Route key={route.path} path={route.path} component={route.component}
+                            exact={route.exact} />
                     ))}
                     <Route exact path="/" render={() => (
-                        <Redirect to="/dashboard"/>
-                    )}/>
-                    </div>
+                        <Redirect to="/dashboard" />
+                    )} />
                 </div>
-                <div className="w-100 text-center position-fixed" style={{fontSize: '.9rem', color: COLORS.background.grey5, backgroundColor: COLORS.background.lightgrey, bottom:0}}>
+                <div className="w-100 text-center position-fixed" style={{ fontSize: '.9rem', color: COLORS.background.grey5, backgroundColor: COLORS.background.lightgrey, bottom: 0 }}>
                     <NavLink to="/impressum">Impressum / Disclaimer</NavLink>
                 </div>
                 <Feedback />
-            </div> 
+            </div>
         </BrowserRouter>
     </Provider>
 )
