26cbe6af48e1d0c59c55ff7ea5f7297298f01bfb
vashchenkov <salam@aleikum>
2020-06-29 09:43:38 +0200

    tutorial dashboard

--- public/tutorial/src/script.js
+++ public/tutorial/src/script.js
@@ -1,3 +1,38 @@
+let coords = [{
+
+    top:    135,
+    height: 240,
+    width:  615,
+
+  }, {
+
+    top:    88,
+    left:   638,
+    height: 221,
+    width:  626,
+
+  }, {
+
+    top:    0,
+    left:   1264,
+    height: 929,
+    width:  620,
+
+  }, {
+
+    top:    365,
+    left:   637,
+    height: 220,
+    width:  615,
+
+  }, {
+
+    top:    619,
+    height: 250,
+    width:  615,
+
+}];
+
 function resize() {
 
   window.onresize = resize;
@@ -10,42 +45,7 @@ function resize() {
     , dashwrap  = document.getElementById('dashwrap')
     , dashboard = document.getElementById('dashboard');
 
-  let coords = [{
-
-        top: 135,
-        height: 240,
-        width: 615,
-
-      }, {
-
-        top: 88,
-        left: 638,
-        height: 221,
-        width: 626,
-
-      }, {
-
-        top: 0,
-        left: 1264,
-        height: 929,
-        width: 620,
-
-      }, {
-
-        top: 365,
-        left: 637,
-        height: 220,
-        width: 615,
-
-      }, {
-
-        top: 619,
-        height: 250,
-        width: 615,
-
-      }];
-
-  let scale = (width - margin * 2) / imgwidth;
+  let scale = (width - (margin + 15) * 2) / imgwidth;
 
   dashboard.style.width = scale * imgwidth;
 
@@ -53,9 +53,9 @@ function resize() {
 
   for (let i = 0; i < 5; i++) {
 
-    dashwrap.children[i].style.top = coords[i].top * scale + dashrect.top;
-    dashwrap.children[i].style.left = coords[i].left * scale + dashrect.left;
-    dashwrap.children[i].style.width = coords[i].width * scale;
+    dashwrap.children[i].style.top    = coords[i].top    * scale + dashrect.top;
+    dashwrap.children[i].style.left   = coords[i].left   * scale + dashrect.left;
+    dashwrap.children[i].style.width  = coords[i].width  * scale;
     dashwrap.children[i].style.height = coords[i].height * scale;
   }
 }
