291b1cf12c284d50d1331562e882af7dfdc3a55c
vashchenkov <salam@aleikum>
2020-07-13 13:01:07 +0200

    refact

--- src/Progress/Store.js
+++ src/Progress/Store.js
@@ -11,7 +11,7 @@ const moduleIsVisible = module => module.code < 200 || module.code >= 400
 const dashboardData = () => {
 
     function erfuellt(array) {
-        return array.reduce((acc, el) => {if (el.erfuellt) return acc + 1})
+        return array.reduce((acc, el) => if (el.erfuellt) return acc + 1)
     }
 
     let data = _.flow([
