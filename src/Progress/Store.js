3d1e1b0f9d402d933b0112f2a7971f368f610c40
vashchenkov <salam@aleikum>
2020-07-13 10:42:59 +0200

    refact

--- src/Progress/Store.js
+++ src/Progress/Store.js
@@ -13,7 +13,10 @@ const getDone = _.sumBy( e => e.erfuellt )
 let temp = ([total, done]) => ({total, done})
 
 //const dashboardData = _.flow([ _.over([getTotal, getDone]), temp])
-const dashboardData = () => _.flow([ _.over([getTotal, getDone]), temp])
+const dashboardData = (data) => {
+    console.log(data)
+    _.flow([ _.over([getTotal, getDone]), temp])
+}
 
 export const selectors = baseStore.withLoadedSelector({
     getTree: state => baseStore.getItems(state),
