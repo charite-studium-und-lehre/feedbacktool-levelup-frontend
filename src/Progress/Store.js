98e77097734b7aedcaab5e242b65ef98dd0e62d6
vashchenkov <salam@aleikum>
2020-07-13 15:44:13 +0200

    refact

--- src/Progress/Store.js
+++ src/Progress/Store.js
@@ -6,10 +6,25 @@ export const identifier = 'progress'
 
 const baseStore = BaseStore(identifier)
 
+var groupBy = function(data, key) {
+  return data.reduce(function(storage, item) {
+    var group = item[key];
+
+    storage[group] = storage[group] || [];
+
+    storage[group].push(item);
+
+    return storage;
+  }, {});
+};
+
 function trans(data) {
 
-    console.log(data);
+    let out = {};
 
+    data = data.meilensteine;
+
+    console.log(groupBy(data, 'fachsemester');
 }
 
 const transform = _.flow([
