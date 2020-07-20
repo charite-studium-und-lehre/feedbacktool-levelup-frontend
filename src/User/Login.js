10f8aabf633614ad17574d38f77c8ce62f4a5dcf
Toma <m.towma@hotmail.com>
2020-07-20 18:56:05 +0200

    Version 05

--- src/User/Login.js
+++ src/User/Login.js
@@ -4,10 +4,11 @@ import _ from 'lodash/fp'
 import backgroundImage from './loginImage.jpg'
 import backgroundImageMobil from './loginImageMobil.jpg'
 import { Redirect } from 'react-router-dom'
-import { withTranslation } from 'react-i18next'
 import makeExtendable from '../Core/makeExtendable'
 import { selectors as user } from './Store'
 import Navbar from '../Core/routing/navbar'
+import Colors from '../colors'
+
 
 const background = (image) => ({
     background: `url(${image}) no-repeat center`,
@@ -25,21 +26,19 @@ const stateToProps = state => ({
 
 const Login = _.compose([
     connect(stateToProps),
-    withTranslation(),
     makeExtendable(),
     C => props => props.loggedIn && !props.hasStammdata ? <Redirect to='/user/registration' /> : <C {...props} />,
     C => props => props.loggedIn ? <Redirect to='/dashboard' /> : <C {...props} />
-])(({ t }) =>
+])(() =>
     <div className='container-fluid h-100' style={background(window.innerWidth < 942 ? backgroundImage : backgroundImageMobil)}>
         <Navbar />
         <div className='row'>
-            <div className='col-12 text-center mb-5 levelup'  >
-                {/* <img className='m-0 p-0 ' src={LevelUp} alt="Levelup" style={{ width: '33rem', height: '' }}></img> */}
-                <span style={{ color: '#B1B0B2', }} >Level</span>
-                <span >Up</span>
+            <div className='col-12 text-center mb-5 levelup'>
+                <span style={{ color:Colors.levelupLogo }}>Level</span>
+                <span>Up</span>
             </div>
             <div className='col-12 col-xl-8 m-auto my-5' style={{transform:'translateY(3em)'}} >
-                <div className='row my-5 '>
+                <div className='row my-5'>
                     <Button col='col-10 col-md-6 m-auto' title='Login' href='https://levelup.charite.de/backend/login' />
                     <Button col='col-10 col-md-6 m-auto' title='Registrieren' href='https://levelup.charite.de/backend/login' />
                 </div>
