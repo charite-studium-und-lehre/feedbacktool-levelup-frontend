3a29a54b2bc36ffd9edba65e0707d264f9875f5e
Toma <m.towma@hotmail.com>
2020-07-26 17:07:28 +0200

    _.default raus

--- src/Dashboard/Strengths.js
+++ src/Dashboard/Strengths.js
@@ -10,7 +10,8 @@ import DashboardCard from './DashboardCard'
 
 const loaded = _.overEvery([ mcSelectors.loaded, ptmSelectors.loaded ])
 const load = () => _.over([ mcActions.load(), ptmActions.load ])
-const ptmStrongestSubject = _.flow([ ptmSelectors.getLatest, _.defaults({}), ptmSelectors.strongestSubject ])
+const ptmStrongestSubject = _.flow([ ptmSelectors.getLatest, ptmSelectors.strongestSubject ])
+
 const stateToProps = state => ({ 
     mcStrongestSubject: mcSelectors.strongestSubject(state), 
     ptmStrongestSubject: ptmStrongestSubject(state),
