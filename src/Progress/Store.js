651bff11edba65f0aadfd3bb7f513bf003d9f390
vashchenkov <salam@aleikum>
2020-08-14 16:54:56 +0200

    refact

--- src/Progress/Store.js
+++ src/Progress/Store.js
@@ -7,17 +7,6 @@ const baseStore = BaseStore(identifier)
 
 function moduleIsVisible(module) { return module.code < 200 || module.code >= 400; }
 
-function getDone(data) {
-
-    let count = 0;
-
-    data.forEach(entry => {
-        if (entry.erfuellt) count++;
-    });
-
-    return count;
-}
-
 function groupBy(data, key) {
 
     return data.reduce((storage, item) => {
@@ -30,22 +19,21 @@ function groupBy(data, key) {
     }, []);
 }
 
-function transform(data) {
+function getStudienfortschrittDisplayData(studienleistungen) {
 
-    data = data.meilensteine;
-    data = groupBy(data, 'fachsemester');
+    studienleistungen = studienleistungen.meilensteine;
+    studienleistungen = groupBy(studienleistungen, 'fachsemester');
 
-    let output = [];
+    let studienfortschrittDisplayData = [];
 
-    data.forEach(element => {
+    studienleistungen.forEach(element => {
 
         let object = {};
-        let bedingung = element.find(d => d.code === element[0].fachsemester + 300);
+        let zulassungsVoraussetzungFuerStudienleistung = element.find(d => d.code === element[0].fachsemester + 300);
 
         object.label = element[0].fachsemester + '. Fachsemester';
 
-        if (bedingung && !bedingung.erfuellt) object.prereq = false;
-        else object.prereq = true;
+        object.prereq = !zulassungsVoraussetzungFuerStudienleistung || zulassungsVoraussetzungFuerStudienleistung.erfuellt;
 
         object.completed = element.find(d => d.code === element[0].fachsemester + 200).erfuellt;
 
@@ -56,32 +44,35 @@ function transform(data) {
                 link: (d.format && `/exams/${d.format}s/${d.studiPruefungsId}`)
             }));
 
-        output.push(object);
+        studienfortschrittDisplayData.push(object);
     })
 
-    return output;
+    return studienfortschrittDisplayData;
 }
 
-function dashboard(state) {
+function getStudienfortschrittDashboardData(state) {
 
-    let temp = baseStore.getItems(state);
+    let studienleistungen = baseStore.getItems(state);
 
-    temp = temp.flatMap(d => d.entries);
-    temp = temp.filter(moduleIsVisible);
+    studienleistungen = studienleistungen.flatMap(d => d.entries);
+    studienleistungen = studienleistungen.filter(moduleIsVisible);
 
-    return {total: temp.length, done: getDone(temp)};
+    let done = 0;
+    studienleistungen.forEach(element => element.erfuellt ? done++ : 0);
+
+    return {total: studienleistungen.length, done: done};
 }
 
 export const selectors = baseStore.withLoadedSelector({
     getTree: state => baseStore.getItems(state),
-    getDashboardData: dashboard,
+    getDashboardData: getStudienfortschrittDashboardData,
 })
 
 export const reducer = combineReducers(baseStore.withLoadedReducer(
     (state = {}, action) => {
         switch(action.type) {
             case `${identifier.toUpperCase()}_DATA_FETCHED`:
-                return transform(action.payload)
+                return getStudienfortschrittDisplayData(action.payload)
             default:
                 return state
         }
