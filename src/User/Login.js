f38dfd20009fcb610f99424b3f9305c459122f4e
vashchenkov <salam@aleikum>
2020-08-24 11:05:31 +0200

    tutorial

--- src/User/Login.js
+++ src/User/Login.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux'
 import _ from 'lodash/fp'
 import image from './login.jpg'
 import {Link, Redirect} from 'react-router-dom'
-import {withTranslation} from 'react-i18next'
 import makeExtendable from '../Core/makeExtendable'
 import {selectors as user} from './Store'
 
@@ -19,43 +18,42 @@ const stateToProps = state => ({
 
 const Login = _.compose([
     connect(stateToProps),
-    withTranslation(),
     makeExtendable(),
     C => props => props.loggedIn && !props.hasStammdata ? <Redirect to='/user/registration'/> : <C {...props} />,
     C => props => props.loggedIn ? <Redirect to='/dashboard'/> : <C {...props} />
-])(({t}) =>
+])(() =>
     <div className='container-fluid h-100'>
         <div className='row h-100' style={background}>
             <div className='col-md-5 offset-md-7 bg-white px-3'>
-                <h3 className='my-4 text-center'>{t(`Willkommen bei LevelUp`)}</h3>
+                <h3 className='my-4 text-center'>{`Willkommen bei LevelUp`}</h3>
                 <div className='mb-3'>
-                    <p>{t(`Herzlich Willkommen bei LevelUp – deinem Online Feedback Tool zur integrierten Darstellung deines Studienfortschritts im Modellstudiengang 2.0.`)}</p>
+                    <p>{`Herzlich Willkommen bei LevelUp – deinem Online Feedback Tool zur integrierten Darstellung deines Studienfortschritts im Modellstudiengang 2.0.`}</p>
                 </div>
                 <Link to='/user/dataProtection'>
                     <div className='color-navigation'>
                         <span className='font-weight-bold'
-                              style={{cursor: 'pointer', fontSize: '1rem'}}>{t(`Datenschutzhinweise`)}</span>
+                              style={{cursor: 'pointer', fontSize: '1rem'}}>{`Datenschutzhinweise`}</span>
                     </div>
                 </Link>
                 <div className="container-form my-3 px-4">
                     <a href="https://levelup.charite.de/backend/login">
-                        <button type="button" className="btn btn-secondary w-100 font-weight-bold">{t(`Login`)}</button>
+                        <button type="button" className="btn btn-secondary w-100 font-weight-bold">{`Login`}</button>
                     </a>
                 </div>
                 <div className='mb-3'>
-                    <div>{t('Bei Problemen mit dem Login melde dich bitte bei levelup@charite.de.')}</div>
+                    <div>{'Bei Problemen mit dem Login melde dich bitte bei levelup@charite.de.'}</div>
                 </div>
                 <div className='color-navigation'>
                     <a href="https://levelup.charite.de/app-demo" className='font-weight-bold' style={{
                         cursor: 'pointer',
                         fontSize: '1rem'
-                    }}>{t(`Demo-Version von LevelUp ansehen`)}</a>
+                    }}>{`Demo-Version von LevelUp ansehen`}</a>
                 </div>
                 <div className='color-navigation'>
                     <a href="tutorial/src/index.html" className='font-weight-bold' style={{
                         cursor: 'pointer',
                         fontSize: '1rem'
-                    }}>{t(`Tutorial ansehen`)}</a>
+                    }}>{`Tutorial ansehen`}</a>
                 </div>
             </div>
         </div>
