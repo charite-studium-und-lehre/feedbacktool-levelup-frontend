9bd92b676db6601aa59adac6a658bca061abd760
vashchenkov <salam@aleikum>
2020-10-19 01:09:19 +0200

    refactoring

---
+++ src/Tutorial/Tutorial.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import script from './tutorial/src/script.js'
+import Dashboard from './tutorial/src/dashboard.js'
+import ÄrtzlicheTätigkeiten from './tutorial/src/arzt-taet.js'
+import Fortschritt from './tutorial/src/fortschritt.js'
+import StärkeSchwäche from './tutorial/src/staerke-schwaeche.js'
+import Timeline from './tutorial/src/timeline.js'
+import TrainingMC from './tutorial/src/training-mc.js'
+import './tutorial/src/styles.css'
+
+
+export default () => {
+  return <Dashboard/>
+}
