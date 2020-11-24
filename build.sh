aa7ff99600efed9418fe275b5d2e3f7a3e8634dc
Dittmar, Martin <martin.dittmar@charite.de>
2020-11-24 14:18:34 +0100

    Build-Skript muss install *und* upgrade aufrufen

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo -e "  ---  Build für Verzeichnis \e[44m\e[97m $DIR \e[0m - Git-Branch \e[44m\e[97m $GIT_BRANCH \e[0m ---   "
 
 DATUM=`date '+%d.%m.%Y %H:%M'`
+yarn install
 yarn upgrade
 
 echo Ausführen von:    PUBLIC_URL="/$DIR" REACT_APP_SITE_TITLE="Branch $GIT_BRANCH, Deployment am $DATUM" yarn build
