5a085563896714d3b62bf64532943c0e306aef5f
vashchenkov <salam@aleikum>
2020-12-20 20:10:07 +0100

    refactoring

--- src/Exams/MainChart.js
+++ src/Exams/MainChart.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import _ from 'lodash/fp'
 import needsData from '../Core/needsData'
 import { OrdinalChart } from '../Charting/Chart'
 import PointGraph from '../Charting/PointGraph'
@@ -17,11 +16,11 @@ const MainChart = ({ graphs, history, fromQuery = { id: -1 }, selected, setSelec
     return <div style={{height: '2.5rem', minWidth: `${semesters.length*8}rem`}}>
         {semesters.length ? <OrdinalChart xDomain={semesters} yDomain={[0,1]}>
             <XAxis />
-            {graphs.map((g, i) => 
+            {graphs.map((g, i) =>
                 <PointGraph
                     offset={i / (graphs.length - 1)}
                     onClick={p => history.push(`/exams/${p.link}`) }
-                    key={i} data={g.data} 
+                    key={i} data={g.data}
                     color={g.color} />
             )}
         </OrdinalChart> :
@@ -30,11 +29,19 @@ const MainChart = ({ graphs, history, fromQuery = { id: -1 }, selected, setSelec
     </div>
 }
 
-const stateToProps = (state, ownProps) => ({ 
-    graphs: selectors.getNavigationData(state), 
+const stateToProps = (state, ownProps) => ({
+    graphs: selectors.getNavigationData(state),
     fromQuery: selectors.getById(state, ownProps.match.params.id),
     selected: selectors.getSelected(state),
     semesters: selectors.getSemesters(state),
 })
 
-export default _.compose(withRouter, needsData(selectors.loaded, actions.load), connect(stateToProps, actions))(MainChart)
+export default function(data) {
+
+    let compose = connect(stateToProps, actions)(MainChart)
+    compose = needsData(selectors.loaded, actions.load)(compose);
+    compose = withRouter(compose);
+
+    return compose(data);
+}
+
