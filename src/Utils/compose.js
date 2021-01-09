f12090b41bedd3bb76242ecd0b0d3c2a122e3eb1
vashchenkov <salam@aleikum>
2021-01-09 22:04:41 +0100

    refactoring

--- src/Utils/compose.js
+++ src/Utils/compose.js
@@ -1,5 +1,11 @@
-export const compose = (...fns) =>
-  fns.reduceRight((prevFn, nextFn) =>
-    (...args) => nextFn(prevFn(...args)),
-    value => value
-)
+export const compose = (...functions) => (parameters) => {
+
+    functions = functions.flat();
+
+    let out = functions[functions.length - 1](parameters);
+
+    for (let i = functions.length - 2; i >= 0; i--)
+        out = functions[i](out);
+
+    return out;
+}
