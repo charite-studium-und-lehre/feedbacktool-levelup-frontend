f4175a5402da81e00ea514725a90b68e04ec0186
vashchenkov <salam@aleikum>
2021-01-10 00:28:08 +0100

    refactoring

--- src/Utils/groupBy.js
+++ src/Utils/groupBy.js
@@ -1,6 +1,13 @@
 export function groupBy(xs, key) {
-  return xs.reduce(function(rv, x) {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
-    return rv;
-  }, []);
+    return xs.reduce(function(rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, []);
+}
+
+export function groupByConsumer(xs, func) {
+    return xs.reduce(function(rv, x) {
+        (rv[func(x)] = rv[func(x)] || []).push(x);
+        return rv;
+    }, []).filter(el => el != null);
 }
