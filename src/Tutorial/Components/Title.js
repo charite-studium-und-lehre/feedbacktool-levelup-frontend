10ca92820069b9ff753f798e9be3f7e43f93049c
towma <46755251+towma@users.noreply.github.com>
2021-01-21 14:39:49 +0100

    fixing

--- src/Tutorial/Components/Title.js
+++ src/Tutorial/Components/Title.js
@@ -1,7 +1,7 @@
 import React from 'react' 
 
 const Title = props =>
-    <div className="pt-3 pb-2 mb-4 row " style={{ boxShadow: '0 4px 5px -5px gray', backgroundColor:'red' }}>
+    <div className="pt-3 pb-2 mb-4 row " style={{ boxShadow: '0 4px 5px -5px gray' }}>
         <div className="col-11 col-lg-10  mx-auto ">
         <p className=" font-weight-bold" style={{ fontSize: window.innerWidth < 1025  ? '0.6': '1.2rem', textAlign:'center' }}>
             {props.text}
