21aab26870f67113bc00e8cb274e46e25d808d89
vashchenkov <salam@aleikum>
2021-06-25 17:27:33 +0200

    refactoring Charting

--- src/Charting/AnimatedInteger.js
+++ src/Charting/AnimatedInteger.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import _ from 'lodash/fp'
+import { uniqueId } from 'lodash/fp'
 import { select } from 'd3-selection'
 import { interpolateNumber } from 'd3-interpolate'
 import { easeQuadInOut } from 'd3-ease'
 import { animationTime as at } from './Utils'
+import { compose } from '../Utils/utils'
 
-const AnimatedInteger = ({ ease = easeQuadInOut, initial, animationTime = at, ...props }) => {
+const AnimatedInteger = ({
+    ease = easeQuadInOut,
+    initial,
+    animationTime = at,
+    ...props
+}) => {
     const [value, setValue] = useState(initial === undefined ? props.value : initial)
-    
+
     useEffect(() => {
         const interpolator = interpolateNumber(value, props.value)
-        const id = _.uniqueId()
+        const id = uniqueId++
         select('body').transition(id)
             .ease(ease)
-            .tween('text', () => _.compose(setValue, Math.round, interpolator))
+            .tween('text', () => compose(setValue, Math.round, interpolator))
             .duration(animationTime)
-        return () => { select('body').interrupt(id) }
-    }, [props.value])
+        return () => { select('boy').interrupt(id) }
+    }, [props.value, value, animationTime, ease])
 
     return <>{value}</>
 }
