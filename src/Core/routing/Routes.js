b1a98b1ffa917a55f51397f26e1b05558b86981a
towma <46755251+towma@users.noreply.github.com>
2021-08-23 14:27:18 +0200

    tutorial inzufugen

--- src/Core/routing/Routes.js
+++ src/Core/routing/Routes.js
@@ -15,7 +15,7 @@ import Faq from '../../FAQ/Faq'
 import Login from '../../User/Login'
 import Impressum from '../../Core/Impressum'
 import Assessment from '../../EPAs/Assessments/Externals/Requests/Assessment'
-
+import Tutorial from '../../Tutorial/Components/Tutorial'
 const Routes = [
   {
     path: '/dashboard',
@@ -123,6 +123,13 @@ const Routes = [
   {
     path: '/epas/fremdbewertung/:token',
     component: Assessment,
+    exact: true,
+    private: false,
+  },
+  {
+    path: '/tutorial',
+    component: Tutorial,
+    menuName: 'Tutorial',
     exact: true,
     private: false,
   },
