b7979946187fbdefb4b4fd93f551dca6ea61cbe6
sascha <salam@aleikum>
2021-09-08 12:35:14 +0200

    Strength refactoring

--- src/Strengths/Subjects.js
+++ src/Strengths/Subjects.js
@@ -1,36 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import _ from 'lodash/fp'
 import SubjectsTabs from '../Utils/Tabs'
 import needsData from '../Core/needsData'
 import Subject from './Subject'
 import { selectors, actions } from './Store'
-import { compose, flow } from '../Utils/utils.js'
 
-const Subjects = compose([needsData(selectors.loaded, actions.load), connect(selectors.getData)])(({subjects, flash}) => {
+const Sub = ({subjects, flash}) => {
+
     let categories = subjects.reduce((map, elm) => {
         let titel = elm.ptm ? elm.ptm.gruppe : 'Andere'
         if (Array.isArray(map.get(titel))) map.get(titel).push(elm)
         else map.set(titel, new Array(elm))
         return map
     }, new Map())
+
     categories = Object.fromEntries(categories)
-    return (
-    <SubjectsTabs>
-        {flow(_.entries, _.reverse, _.map(([cat, subs]) =>
-            <div key={cat} title={cat} className="d-flex flex-wrap justify-content-around">{
-                _.sortBy('titel', subs).map(
-                    s =>
+
+    return (<SubjectsTabs>
+        {
+            Object.entries(categories).reverse().map(([cat, subs]) =>
+                <div key={cat} title={cat} className="d-flex flex-wrap justify-content-around">
+                {
+                    subs.sort((a, b) => ('' + a.titel).localeCompare(b.titel)).map(s =>
                         <Subject
                             data={[s.mc || {}, s.ptm || {}]}
                             key={s.titel}
                             titel={s.titel}
                             flash={s.titel === flash}
-                        />)
+                        />
+                    )
                 }
-            </div>
-        ))(categories)}
-    </SubjectsTabs>
-)})
+                </div>
+            )
+        }
+    </SubjectsTabs>)
+}
 
-export default Subjects
+export default needsData(selectors.loaded, actions.load)(connect(selectors.getData)(Sub))
